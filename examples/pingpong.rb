f9e639c14c4a6c34746f959eb6f9f4e93535dc74
Aman Gupta <aman@tmm1.net>
2008-07-18 19:41:17 -0700

    simplify pingpong example

--- examples/pingpong.rb
+++ examples/pingpong.rb
@@ -10,44 +10,40 @@ EM.run{
   # AMQP.logging = true
 
   amq = MQ.new
+  EM.add_periodic_timer(1){
+    puts
+
+    log :sending, 'ping'
+    amq.queue('one').publish('ping')
+  }
+
+  amq = MQ.new
   amq.queue('one').subscribe{ |headers, msg|
-    log 'one', :received, msg, :from => headers.reply_to
-
-    if headers.reply_to
-      msg[1] = 'o'
-
-      log 'one', :sending, msg, :to => headers.reply_to
-      amq.direct.publish(msg, :key => headers.reply_to)
-    else
-      puts
-    end
+    log 'one', :received, msg
+    log 'one', :sending, 'pong'
+    amq.queue('two').publish('pong')
   }
   
   amq = MQ.new
   amq.queue('two').subscribe{ |msg|
     log 'two', :received, msg
-    puts
-  }
-
-  amq = MQ.new
-  amq.direct.publish('ding', :key => 'one')
-  EM.add_periodic_timer(1){
-    log :sending, 'ping', :to => 'one', :from => 'two'
-    amq.direct.publish('ping', :key => 'one', :reply_to => 'two')
   }
 
 }
 
 __END__
 
-[Thu Jul 17 21:23:55 -0700 2008, "one", :received, "ding", {:from=>nil}]
+[Fri Jul 18 19:34:39 -0700 2008, :sending, "ping"]
+[Fri Jul 18 19:34:39 -0700 2008, "one", :received, "ping"]
+[Fri Jul 18 19:34:39 -0700 2008, "one", :sending, "pong"]
+[Fri Jul 18 19:34:39 -0700 2008, "two", :received, "pong"]
 
-[Thu Jul 17 21:23:56 -0700 2008, :sending, "ping", {:from=>"two", :to=>"one"}]
-[Thu Jul 17 21:23:56 -0700 2008, "one", :received, "ping", {:from=>"two"}]
-[Thu Jul 17 21:23:56 -0700 2008, "one", :sending, "pong", {:to=>"two"}]
-[Thu Jul 17 21:23:56 -0700 2008, "two", :received, "pong"]
+[Fri Jul 18 19:34:40 -0700 2008, :sending, "ping"]
+[Fri Jul 18 19:34:40 -0700 2008, "one", :received, "ping"]
+[Fri Jul 18 19:34:40 -0700 2008, "one", :sending, "pong"]
+[Fri Jul 18 19:34:40 -0700 2008, "two", :received, "pong"]
 
-[Thu Jul 17 21:23:57 -0700 2008, :sending, "ping", {:from=>"two", :to=>"one"}]
-[Thu Jul 17 21:23:57 -0700 2008, "one", :received, "ping", {:from=>"two"}]
-[Thu Jul 17 21:23:57 -0700 2008, "one", :sending, "pong", {:to=>"two"}]
-[Thu Jul 17 21:23:57 -0700 2008, "two", :received, "pong"]
+[Fri Jul 18 19:34:41 -0700 2008, :sending, "ping"]
+[Fri Jul 18 19:34:41 -0700 2008, "one", :received, "ping"]
+[Fri Jul 18 19:34:41 -0700 2008, "one", :sending, "pong"]
+[Fri Jul 18 19:34:41 -0700 2008, "two", :received, "pong"]
