5605cea1e8e8b9f11cece475e5c3c2314815c0e8
Aman Gupta <aman@tmm1.net>
2008-07-18 21:08:25 -0700

    make primes example easier to run

--- examples/primes.rb
+++ examples/primes.rb
@@ -11,6 +11,8 @@ EM.run{
 
   if ARGV[0] == 'worker'
 
+    log 'prime checker', :started, :pid => Process.pid
+
     class Fixnum
       def prime?
         ('1' * self) !~ /^1?$|^(11+?)\1+$/
@@ -20,15 +22,11 @@ EM.run{
     MQ.queue('prime checker').subscribe{ |info, num|
       log 'prime checker', :prime?, num, :pid => Process.pid
       if Integer(num).prime?
-        MQ.direct.publish(num, :key => info.reply_to, :reply_to => Process.pid)
+        MQ.queue(info.reply_to).publish(num, :reply_to => Process.pid)
       end
     }
 
-  else
-
-    # define the prime checker queue so messages we send
-    # before starting workers are not lost
-    MQ.queue('prime checker')
+  elsif ARGV[0] == 'controller'
 
     MQ.queue('prime collector').subscribe{ |info, prime|
       log 'prime collector', :received, prime, :from_pid => info.reply_to
@@ -37,9 +35,23 @@ EM.run{
 
     i = 1
     EM.add_periodic_timer(0.01) do
-      MQ.direct.publish(i.to_s, :reply_to => 'prime collector', :key => 'prime checker')
+      MQ.queue('prime checker').publish(i.to_s, :reply_to => 'prime collector')
       EM.stop_event_loop if i == 50
       i += 1
+    end
+
+  else # run 3 workers and 1 controller as an example
+
+    %w[ worker worker worker controller ].each do |type|
+      EM.popen("ruby #{$0} #{type}") do |c|
+        def c.receive_data data
+          puts data
+        end
+
+        def c.unbind
+          EM.stop_event_loop
+        end
+      end
     end
 
   end
@@ -47,81 +59,71 @@ EM.run{
 
 __END__
 
-$ ruby primes.rb worker &
-[1] 4787
-
-$ ruby primes.rb worker &
-[2] 4791
-
-$ ruby primes.rb worker &
-[3] 4794
-
-$ ruby primes.rb worker &
-[4] 4797
-
-$ ruby primes.rb 
-["prime checker", :prime?, "1", {:pid=>4787}]
-["prime checker", :prime?, "2", {:pid=>4791}]
-["prime collector", :received, "2", {:from_pid=>"4791"}]
-["prime checker", :prime?, "3", {:pid=>4794}]
-["prime collector", :received, "3", {:from_pid=>"4794"}]
-["prime checker", :prime?, "4", {:pid=>4797}]
-["prime checker", :prime?, "5", {:pid=>4787}]
-["prime collector", :received, "5", {:from_pid=>"4787"}]
-["prime checker", :prime?, "6", {:pid=>4791}]
-["prime checker", :prime?, "7", {:pid=>4794}]
-["prime collector", :received, "7", {:from_pid=>"4794"}]
-["prime checker", :prime?, "8", {:pid=>4797}]
-["prime checker", :prime?, "9", {:pid=>4787}]
-["prime checker", :prime?, "10", {:pid=>4791}]
-["prime checker", :prime?, "11", {:pid=>4794}]
-["prime collector", :received, "11", {:from_pid=>"4794"}]
-["prime checker", :prime?, "12", {:pid=>4797}]
-["prime checker", :prime?, "13", {:pid=>4787}]
-["prime collector", :received, "13", {:from_pid=>"4787"}]
-["prime checker", :prime?, "14", {:pid=>4791}]
-["prime checker", :prime?, "15", {:pid=>4794}]
-["prime checker", :prime?, "16", {:pid=>4797}]
-["prime checker", :prime?, "17", {:pid=>4787}]
-["prime collector", :received, "17", {:from_pid=>"4787"}]
-["prime checker", :prime?, "18", {:pid=>4791}]
-["prime checker", :prime?, "19", {:pid=>4794}]
-["prime collector", :received, "19", {:from_pid=>"4794"}]
-["prime checker", :prime?, "20", {:pid=>4797}]
-["prime checker", :prime?, "21", {:pid=>4787}]
-["prime checker", :prime?, "22", {:pid=>4791}]
-["prime checker", :prime?, "23", {:pid=>4794}]
-["prime collector", :received, "23", {:from_pid=>"4794"}]
-["prime checker", :prime?, "24", {:pid=>4797}]
-["prime checker", :prime?, "25", {:pid=>4787}]
-["prime checker", :prime?, "26", {:pid=>4791}]
-["prime checker", :prime?, "27", {:pid=>4794}]
-["prime checker", :prime?, "28", {:pid=>4797}]
-["prime checker", :prime?, "29", {:pid=>4787}]
-["prime collector", :received, "29", {:from_pid=>"4787"}]
-["prime checker", :prime?, "30", {:pid=>4791}]
-["prime checker", :prime?, "31", {:pid=>4794}]
-["prime collector", :received, "31", {:from_pid=>"4794"}]
-["prime checker", :prime?, "32", {:pid=>4797}]
-["prime checker", :prime?, "33", {:pid=>4787}]
-["prime checker", :prime?, "34", {:pid=>4791}]
-["prime checker", :prime?, "35", {:pid=>4794}]
-["prime checker", :prime?, "36", {:pid=>4797}]
-["prime checker", :prime?, "37", {:pid=>4787}]
-["prime collector", :received, "37", {:from_pid=>"4787"}]
-["prime checker", :prime?, "38", {:pid=>4791}]
-["prime checker", :prime?, "39", {:pid=>4794}]
-["prime checker", :prime?, "40", {:pid=>4797}]
-["prime checker", :prime?, "41", {:pid=>4787}]
-["prime collector", :received, "41", {:from_pid=>"4787"}]
-["prime checker", :prime?, "42", {:pid=>4791}]
-["prime checker", :prime?, "43", {:pid=>4794}]
-["prime collector", :received, "43", {:from_pid=>"4794"}]
-["prime checker", :prime?, "44", {:pid=>4797}]
-["prime checker", :prime?, "45", {:pid=>4787}]
-["prime checker", :prime?, "46", {:pid=>4791}]
-["prime checker", :prime?, "47", {:pid=>4794}]
-["prime collector", :received, "47", {:from_pid=>"4794"}]
-["prime checker", :prime?, "48", {:pid=>4797}]
-["prime checker", :prime?, "49", {:pid=>4787}]
-["prime checker", :prime?, "50", {:pid=>4791}]
+["prime checker", :started, {:pid=>1958}]
+["prime checker", :started, {:pid=>1957}]
+["prime checker", :started, {:pid=>1956}]
+["prime checker", :prime?, "1", {:pid=>1958}]
+["prime checker", :prime?, "2", {:pid=>1957}]
+["prime collector", :received, "2", {:from_pid=>"1957"}]
+["prime checker", :prime?, "3", {:pid=>1956}]
+["prime collector", :received, "3", {:from_pid=>"1956"}]
+["prime checker", :prime?, "4", {:pid=>1958}]
+["prime checker", :prime?, "5", {:pid=>1957}]
+["prime collector", :received, "5", {:from_pid=>"1957"}]
+["prime checker", :prime?, "6", {:pid=>1956}]
+["prime checker", :prime?, "7", {:pid=>1958}]
+["prime collector", :received, "7", {:from_pid=>"1958"}]
+["prime checker", :prime?, "8", {:pid=>1957}]
+["prime checker", :prime?, "9", {:pid=>1956}]
+["prime checker", :prime?, "10", {:pid=>1958}]
+["prime checker", :prime?, "11", {:pid=>1957}]
+["prime collector", :received, "11", {:from_pid=>"1957"}]
+["prime checker", :prime?, "12", {:pid=>1956}]
+["prime checker", :prime?, "13", {:pid=>1958}]
+["prime collector", :received, "13", {:from_pid=>"1958"}]
+["prime checker", :prime?, "14", {:pid=>1957}]
+["prime checker", :prime?, "15", {:pid=>1956}]
+["prime checker", :prime?, "16", {:pid=>1958}]
+["prime checker", :prime?, "17", {:pid=>1957}]
+["prime collector", :received, "17", {:from_pid=>"1957"}]
+["prime checker", :prime?, "18", {:pid=>1956}]
+["prime checker", :prime?, "19", {:pid=>1958}]
+["prime collector", :received, "19", {:from_pid=>"1958"}]
+["prime checker", :prime?, "20", {:pid=>1957}]
+["prime checker", :prime?, "21", {:pid=>1956}]
+["prime checker", :prime?, "22", {:pid=>1958}]
+["prime checker", :prime?, "23", {:pid=>1957}]
+["prime collector", :received, "23", {:from_pid=>"1957"}]
+["prime checker", :prime?, "24", {:pid=>1956}]
+["prime checker", :prime?, "25", {:pid=>1958}]
+["prime checker", :prime?, "26", {:pid=>1957}]
+["prime checker", :prime?, "27", {:pid=>1956}]
+["prime checker", :prime?, "28", {:pid=>1958}]
+["prime checker", :prime?, "29", {:pid=>1957}]
+["prime collector", :received, "29", {:from_pid=>"1957"}]
+["prime checker", :prime?, "30", {:pid=>1956}]
+["prime checker", :prime?, "31", {:pid=>1958}]
+["prime collector", :received, "31", {:from_pid=>"1958"}]
+["prime checker", :prime?, "32", {:pid=>1957}]
+["prime checker", :prime?, "33", {:pid=>1956}]
+["prime checker", :prime?, "34", {:pid=>1958}]
+["prime checker", :prime?, "35", {:pid=>1957}]
+["prime checker", :prime?, "36", {:pid=>1956}]
+["prime checker", :prime?, "37", {:pid=>1958}]
+["prime collector", :received, "37", {:from_pid=>"1958"}]
+["prime checker", :prime?, "38", {:pid=>1957}]
+["prime checker", :prime?, "39", {:pid=>1956}]
+["prime checker", :prime?, "40", {:pid=>1958}]
+["prime checker", :prime?, "41", {:pid=>1957}]
+["prime collector", :received, "41", {:from_pid=>"1957"}]
+["prime checker", :prime?, "42", {:pid=>1956}]
+["prime checker", :prime?, "43", {:pid=>1958}]
+["prime collector", :received, "43", {:from_pid=>"1958"}]
+["prime checker", :prime?, "44", {:pid=>1957}]
+["prime checker", :prime?, "45", {:pid=>1956}]
+["prime checker", :prime?, "46", {:pid=>1958}]
+["prime checker", :prime?, "47", {:pid=>1957}]
+["prime collector", :received, "47", {:from_pid=>"1957"}]
+["prime checker", :prime?, "48", {:pid=>1956}]
+["prime checker", :prime?, "49", {:pid=>1958}]
+["prime checker", :prime?, "50", {:pid=>1957}]
