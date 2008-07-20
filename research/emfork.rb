8137de94914753480ebc29433aa0d9cd2dbc821c
Aman Gupta <aman@tmm1.net>
2008-07-20 06:32:33 -0700

    EM.fork from within a running reactor

--- research/emfork.rb
+++ research/emfork.rb
@@ -3,8 +3,6 @@ require 'eventmachine'
 
 # helper to fork off EM reactors
 def EM.fork num = 1, &blk
-  raise 'Cannot fork while reactor is running' if reactor_running?
-
   unless @forks
     trap('CHLD'){
       pid = Process.wait
@@ -26,13 +24,28 @@ def EM.fork num = 1, &blk
   num.times do
     pid = Kernel.fork do
       def EM.is_fork?() true end
+
       p [:pid, Process.pid, :started, is_fork?]
       trap('USR1'){ EM.stop_event_loop }
+
+      p [:reactor_running, EM.reactor_running?]
+      if EM.reactor_running?
+        EM.stop_event_loop
+        EM.release_machine
+      end
+      # EM doesn't reset reactor_running
+      def EM.reactor_running?
+        @reactor_running = false
+      end
+      p [:reactor_running, EM.reactor_running?]
+
+      p 'starting new reactor'
       EM.run(&blk)
-      exit
+      p 'new reactor stopped'
     end
 
     @forks[pid] = blk
+    p [:children, EM.forks]
   end
 end
 def EM.is_fork?() false end
@@ -40,8 +53,15 @@ def EM.forks
   @forks.keys
 end
 
-p [:parent, Process.pid]
-EM.fork(2){}
-p [:children, EM.forks]
-Process.wait
+EM.run{
+  p [:parent, Process.pid]
+  EM.fork(2){
+    EM.add_periodic_timer(1) do
+      p [:fork, Process.pid, :ping]
+    end
+  }
+}
+
+# Process.wait
 # EM.run{}
+p 'original reactor stopped'
