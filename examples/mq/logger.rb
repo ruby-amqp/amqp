e45b36f90d05fefe53d005614c3d9e7138d0bb04
Aman Gupta <aman@tmm1.net>
2008-08-13 12:02:41 -0700

    add MQ::Logger

--- examples/mq/logger.rb
+++ examples/mq/logger.rb
@@ -1,74 +1,13 @@
 $:.unshift File.dirname(__FILE__) + '/../../lib'
 require 'mq'
+require 'mq/logger'
 
-class Logger
-  def initialize *args, &block
-    opts = args.pop if args.last.is_a? Hash
-    opts ||= {}
-    printer(block) if block
-    @log = []
-    @tags = ([:timestamp] + args).uniq
-  end
-
-  def log severity, *args
-    opts = args.pop if args.last.is_a? Hash and args.size != 1
-    opts ||= {}
-    data = args.shift
-
-    data = {:type => :exception,
-            :name => data.class.to_s.intern,
-            :backtrace => data.backtrace,
-            :message => data.message} if data.is_a? Exception
-        
-    (@tags + args).each do |tag|
-      tag = tag.to_sym
-      case tag
-      when :timestamp
-        opts.update :timestamp => Time.now.to_i
-      when :hostname
-        @hostname ||= { :hostname => `hostname`.strip }
-        opts.update @hostname
-      when :process
-        @process_id ||= { :process_id => Process.pid,
-                          :process_name => $0,
-                          :process_parent_id => Process.ppid,
-                          :thread_id => Thread.current.object_id }
-        opts.update :process => @process_id
-      else
-        (opts[:tags] ||= []) << tag
-      end
-    end
-    
-    opts.update(:severity => severity,
-                :data => data)
-    
-    print(opts)
-    MQ.queue('logger', :durable => true).publish(Marshal.dump(opts), :persistent => true)
-    opts
-  end
-  alias :method_missing :log
-
-  def print data = nil, &block
-    if block
-      @printer = block
-    elsif data.is_a? Proc
-      @printer = data
-    elsif @printer and data
-      @printer.call(data)
-    else
-      @printer
-    end
-  end
-  alias :printer :print
-end
+Logger = MQ::Logger
 
 EM.run{
-  # AMQP.logging = true
-  # MQ.logging = true
-
   if ARGV[0] == 'server'
 
-    MQ.queue('logger', :durable => true).subscribe{|msg|
+    MQ.queue('logger').bind(MQ.fanout('logging', :durable => true)).subscribe{|msg|
       msg = Marshal.load(msg)
       require 'pp'
       pp(msg)
---
+++ lib/mq/logger.rb
@@ -0,0 +1,63 @@
+class MQ
+  class Logger
+    def initialize *args, &block
+      opts = args.pop if args.last.is_a? Hash
+      opts ||= {}
+      printer(block) if block
+      @log = []
+      @tags = ([:timestamp] + args).uniq
+    end
+
+    def log severity, *args
+      opts = args.pop if args.last.is_a? Hash and args.size != 1
+      opts ||= {}
+      data = args.shift
+
+      data = {:type => :exception,
+              :name => data.class.to_s.intern,
+              :backtrace => data.backtrace,
+              :message => data.message} if data.is_a? Exception
+
+      (@tags + args).each do |tag|
+        tag = tag.to_sym
+        case tag
+        when :timestamp
+          opts.update :timestamp => Time.now.to_i
+        when :hostname
+          @hostname ||= { :hostname => `hostname`.strip }
+          opts.update @hostname
+        when :process
+          @process_id ||= { :process_id => Process.pid,
+                            :process_name => $0,
+                            :process_parent_id => Process.ppid,
+                            :thread_id => Thread.current.object_id }
+          opts.update :process => @process_id
+        else
+          (opts[:tags] ||= []) << tag
+        end
+      end
+
+      opts.update(:severity => severity,
+                  :data => data)
+
+      print(opts)
+      MQ.fanout('logging', :durable => true).publish Marshal.dump(opts)
+
+      opts
+    end
+    alias :method_missing :log
+
+    def print data = nil, &block
+      if block
+        @printer = block
+      elsif data.is_a? Proc
+        @printer = data
+      elsif @printer and data
+        @printer.call(data)
+      else
+        @printer
+      end
+    end
+    alias :printer :print
+  end
+end
