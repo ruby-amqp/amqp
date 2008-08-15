2429a3150e5dfe90961cd64dd1e0b55558c74300
Aman Gupta <aman@tmm1.net>
2008-08-15 01:45:47 -0700

    simplify stocks example; show off custom topic exchanges

--- examples/mq/stocks.rb
+++ examples/mq/stocks.rb
@@ -7,50 +7,52 @@ EM.run{
     p [ Time.now, *args ]
   end
 
-  # AMQP.logging = true
+  def publish_stock_prices
+    mq = MQ.new
+    EM.add_periodic_timer(1){
+      puts
 
-  EM.add_periodic_timer(1){
-    puts
+      { :appl => 170+rand(1000)/100.0,
+        :msft => 22+rand(500)/100.0
+      }.each do |stock, price|
+        stock = "usd.#{stock}"
 
-    log :publishing, 'stock.usd.appl', price = 170+rand(1000)/100.0
-    MQ.topic.publish(price, :key => 'stock.usd.appl', :headers => {:symbol => 'appl'})
+        log :publishing, stock, price
+        mq.topic('stocks').publish(price, :key => stock)
+      end
+    }
+  end
 
-    log :publishing, 'stock.usd.msft', price = 22+rand(500)/100.0
-    MQ.topic.publish(price, :key => 'stock.usd.msft', :headers => {:symbol => 'msft'})
-  }
-
-  Thread.new{
-    amq = MQ.new
-    amq.queue('apple stock').bind(amq.topic, :key => 'stock.usd.appl').subscribe{ |price|
+  def watch_appl_stock
+    mq = MQ.new
+    mq.queue('apple stock').bind(mq.topic('stocks'), :key => 'usd.appl').subscribe{ |price|
       log 'apple stock', price
     }
-  }
+  end
 
-  Thread.new{
-    amq = MQ.new
-    amq.queue('us stocks').bind(amq.topic, :key => 'stock.usd.*').subscribe{ |info, price|
-      log 'us stock', info.headers[:symbol], price
+  def watch_us_stocks
+    mq = MQ.new
+    mq.queue('us stocks').bind(mq.topic('stocks'), :key => 'usd.*').subscribe{ |info, price|
+      log 'us stock', info.routing_key, price
     }
-  }
+  end
+
+  publish_stock_prices
+  watch_appl_stock
+  watch_us_stocks
 
 }
 
 __END__
 
-[Thu Jul 17 14:51:07 -0700 2008, :publishing, "stock.usd.appl", 170.84]
-[Thu Jul 17 14:51:07 -0700 2008, :publishing, "stock.usd.msft", 23.68]
-[Thu Jul 17 14:51:07 -0700 2008, "apple stock", "170.84"]
-[Thu Jul 17 14:51:07 -0700 2008, "us stock", "appl", "170.84"]
-[Thu Jul 17 14:51:07 -0700 2008, "us stock", "msft", "23.68"]
+[Fri Aug 15 01:39:00 -0700 2008, :publishing, "usd.appl", 173.45]
+[Fri Aug 15 01:39:00 -0700 2008, :publishing, "usd.msft", 26.98]
+[Fri Aug 15 01:39:00 -0700 2008, "apple stock", "173.45"]
+[Fri Aug 15 01:39:00 -0700 2008, "us stock", "usd.appl", "173.45"]
+[Fri Aug 15 01:39:00 -0700 2008, "us stock", "usd.msft", "26.98"]
 
-[Thu Jul 17 14:51:08 -0700 2008, :publishing, "stock.usd.appl", 173.61]
-[Thu Jul 17 14:51:08 -0700 2008, :publishing, "stock.usd.msft", 25.8]
-[Thu Jul 17 14:51:08 -0700 2008, "apple stock", "173.61"]
-[Thu Jul 17 14:51:08 -0700 2008, "us stock", "appl", "173.61"]
-[Thu Jul 17 14:51:08 -0700 2008, "us stock", "msft", "25.8"]
-
-[Thu Jul 17 14:51:09 -0700 2008, :publishing, "stock.usd.appl", 173.94]
-[Thu Jul 17 14:51:09 -0700 2008, :publishing, "stock.usd.msft", 24.88]
-[Thu Jul 17 14:51:09 -0700 2008, "apple stock", "173.94"]
-[Thu Jul 17 14:51:09 -0700 2008, "us stock", "appl", "173.94"]
-[Thu Jul 17 14:51:09 -0700 2008, "us stock", "msft", "24.88"]
+[Fri Aug 15 01:39:01 -0700 2008, :publishing, "usd.appl", 179.72]
+[Fri Aug 15 01:39:01 -0700 2008, :publishing, "usd.msft", 26.56]
+[Fri Aug 15 01:39:01 -0700 2008, "apple stock", "179.72"]
+[Fri Aug 15 01:39:01 -0700 2008, "us stock", "usd.appl", "179.72"]
+[Fri Aug 15 01:39:01 -0700 2008, "us stock", "usd.msft", "26.56"]
