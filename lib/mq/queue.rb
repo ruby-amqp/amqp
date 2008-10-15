ef891c816ddbea3b59c313076cb55da24b3c849d
Aman Gupta <aman@tmm1.net>
2008-10-15 15:42:04 -0700

    raise error if subscribe is called twice; clear out status/subscribe callbacks to prevent memory leaks

--- lib/mq/queue.rb
+++ lib/mq/queue.rb
@@ -65,6 +65,9 @@ class MQ
     def subscribe opts = {}, &blk
       @consumer_tag = "#{name}-#{Kernel.rand(999_999_999_999)}"
       @mq.consumers[@consumer_tag] = self
+
+      raise Error, 'already subscribed to the queue' if @on_msg
+
       @on_msg = blk
       @ack = opts[:no_ack] === false
       @mq.callback{
@@ -75,8 +78,9 @@ class MQ
       }
       self
     end
-    
+
     def unsubscribe opts = {}, &blk
+      @on_msg = nil
       @on_cancel = blk
       @mq.callback{
         @mq.send Protocol::Basic::Cancel.new({ :consumer_tag => @consumer_tag }.merge(opts))
@@ -122,9 +126,10 @@ class MQ
       if @on_status
         m, c = declare_ok.message_count, declare_ok.consumer_count
         @on_status.call *(@on_status.arity == 1 ? [m] : [m, c])
+        @on_status = nil
       end
     end
-    
+
     def cancelled
       @on_cancel.call if @on_cancel
       @on_cancel = @on_msg = nil
