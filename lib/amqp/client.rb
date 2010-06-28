1a8a62ef29d44df3d54b0b89da8062e20404ae62
Marek Majkowski <marek@popcount.org>
2010-06-28 15:53:23 +0100

    Make it more idiomatic (via dpw)

--- lib/amqp/client.rb
+++ lib/amqp/client.rb
@@ -216,15 +216,15 @@ module AMQP
     def self.parse_amqp_url(amqp_url)
       uri = URI.parse(amqp_url)
       q = CGI::parse(uri.query || '')
-      raise("amqp:// uri required!") if !['amqp', 'amqps'].include? uri.scheme
+      raise("amqp:// uri required!") unless %w{amqp amqps}.include? uri.scheme
       opts = {}
       opts[:user] = URI.unescape(uri.user) if uri.user
       opts[:pass] = URI.unescape(uri.password) if uri.password
       opts[:vhost] = URI.unescape(uri.path) if uri.path
       opts[:host] = uri.host if uri.host
-      opts[:port] = uri.port ? uri.port : (uri.scheme == "amqps" ? 5671 : 5672)
-      opts[:ssl] = true if uri.scheme == "amqps"
-      opts[:logging] = true if q['logging'][0] == "true"
+      opts[:port] = {"amqp"=>5672, "amqps"=>5671}[uri.scheme]
+      opts[:ssl] = uri.scheme == "amqps"
+      opts[:logging] = q['logging'][0] == "true"
       opts[:timeout] = Integer(q['timeout'][0]) if q['timeout'][0]
       return opts
     end
