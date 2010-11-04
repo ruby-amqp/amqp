3e565c0622e09ca243b7984cd3760e2362a8be56
arvicco <arvitallian@gmail.com>
2010-11-04 16:31:00 +0300

    AMQP class behavior specified

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,25 @@
 $LOAD_PATH << "." unless $LOAD_PATH.include? "." # moronic 1.9.2 breaks things bad
 
 require 'rspec'
-
-#require 'yaml'
+require 'yaml'
 
 def rspec2?
   defined?(RSpec)
 end
 
+amqp_config = File.dirname(__FILE__) + '/amqp.yml'
+
+if File.exists? amqp_config
+  class Hash
+    def symbolize_keys
+      self.inject({}) { |result, (key, value)|
+        new_key = key.is_a?(String) ? key.to_sym : key
+        new_value = value.is_a?(Hash) ? value.symbolize_keys : value
+        result[new_key] = new_value
+        result
+      }
+    end
+  end
+
+  AMQP_OPTS = YAML::load_file(amqp_config).symbolize_keys[:test]
+end
