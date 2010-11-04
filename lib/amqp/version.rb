7c9a190bc1d52ce257e987a6a37fb5ad79adc616
arvicco <arvitallian@gmail.com>
2010-11-04 09:10:33 +0300

    No message

--- lib/amqp/version.rb
+++ lib/amqp/version.rb
@@ -1,6 +1,6 @@
 require 'pathname'
 
 module AMQP
-  VERSION_FILE = Pathname.new(__FILE__).dirname + '../VERSION'   # :nodoc:
+  VERSION_FILE = Pathname.new(__FILE__).dirname + '../../VERSION'   # :nodoc:
   VERSION = VERSION_FILE.exist? ? VERSION_FILE.read.strip : '0.6.8'
 end
