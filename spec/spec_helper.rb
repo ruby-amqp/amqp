2af0a4d7d43f261bae18b54094adf6634d742c11
Michael S. Klishin <michael@novemberain.com>
2011-01-07 00:18:52 +0300

    Rip bacon and em-spec out for now (read on)
     * When both RSpec and Bacon are loaded at the same time, == matcher always passes.
       If RSpec is the way to go, we clearly need to have only RSpec loaded.
    
     * Not sure what fork of em-spec has RSpec support. Leaving for Jakub to decide.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,39 +3,15 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 require "mq"
-require "bacon"
-require "em-spec/bacon"
 
-# Notes about Bacon & EM-spec
-# - Subsequent describe blocks don't work.
-# - Bacon doesn't support any kind of pending specs.
-# - Bacon doesn't support before(:all) hook (see bellow).
-EM.spec_backend = EventMachine::Spec::Bacon
+# EM.spec_backend = EventMachine::Spec::Rspec
 
 # Usage with tracer:
 # 1) Start tracer on a PORT_NUMBER
 # 2) ruby spec/sync_async_spec.rb amqp://localhost:PORT_NUMBER
-if ARGV.first && ARGV.first.match(/^amqps?:/)
-  amqp_url = ARGV.first
-  puts "Using AMQP URL #{amqp_url}"
-else
-  amqp_url = "amqp://localhost"
-end
-
-# Bacon doesn't seem to have some global before hook
-Bacon::Context.class_eval do
-  alias_method :__initialize__, :initialize
-
-  # Let's use Module#define_method, so we can
-  # access local variables defined in parent scopes.
-  define_method(:initialize) do |*args, &block|
-    __initialize__(*args, &block)
-
-    # There's no support for before(:all), so we just
-    # save whatever AMQP.connect returns (we suppose
-    # it isn't nil) and use value = value || connect().
-    self.before do
-      @connected ||= AMQP.connect(amqp_url)
-    end
-  end
-end
+# if ARGV.first && ARGV.first.match(/^amqps?:/)
+#   amqp_url = ARGV.first
+#   puts "Using AMQP URL #{amqp_url}"
+# else
+#   amqp_url = "amqp://localhost"
+# end
