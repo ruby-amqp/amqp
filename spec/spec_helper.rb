bc42cdcc739160d095e973c0d790cb4bb03d75dd
arvicco <arvitallian@gmail.com>
2011-01-07 17:06:06 +0300

    AMQP::Client specs moved in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,3 +19,20 @@ require "mq"
 # else
 #   amqp_url = "amqp://localhost"
 # end
+
+# Shorthand for mocking subject's instance variable
+def subject_mock(name, as_null = false)
+  mock = mock(name)
+  mock.as_null_object if as_null
+  subject.instance_variable_set(name.to_sym, mock)
+  mock
+end
+
+# Returns Header that should be correctly parsed
+def basic_header opts = {}
+  AMQP::Frame::Header.new(
+      AMQP::Protocol::Header.new(
+          AMQP::Protocol::Basic, :priority => 1), opts[:channel] || 0)
+end
+
+
