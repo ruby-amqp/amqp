8547db6af923255a8e3f996c0f5c0c65793031a7
Michael S. Klishin <michael@novemberain.com>
2011-01-18 06:43:45 +0300

    Golden rule of readable specs: instead of "it does X if Y" use context Y: it does X.

--- spec/mq/collection_spec.rb
+++ spec/mq/collection_spec.rb
@@ -32,19 +32,24 @@ describe MQ::Collection do
       @collection.length.should eql(length + 1)
     end
 
-    it "should not add an item to the collection if another item with given name already exists and the name IS NOT nil" do
-      @collection << Item.new("test")
-      length = @collection.length
-      @collection << Item.new("test")
-      @collection.length.should eql(length)
-    end
+    context "when another item with given name already exists and the name IS NOT nil" do
+      it "should not add an item to the collection" do
+        @collection << Item.new("test")
+        length = @collection.length
+        @collection << Item.new("test")
+        @collection.length.should eql(length)
+      end # it
+    end # context
 
-    it "should add an item to the collection if another item with given name already exists and the name IS nil" do
-      @collection << Item.new(nil)
-      length = @collection.length
-      @collection << Item.new(nil)
-      @collection.length.should eql(length + 1)
-    end
+
+    context "when another item with given name already exists and the name IS nil" do
+      it "should add an item to the collection" do
+        @collection << Item.new(nil)
+        length = @collection.length
+        @collection << Item.new(nil)
+        @collection.length.should eql(length + 1)
+      end # it
+    end # context
 
     it "should return the item" do
       item = Item.new("test")
@@ -68,12 +73,17 @@ describe MQ::Collection do
       end
     end
 
-    it "should return the item even if it already existed" do
-      item = Item.new("test")
-      @collection << item
-      (@collection << item).should eql item
-    end
-  end
+    context "when item we add already exists in the collection" do
+      it "should return the item" do
+        item = Item.new("test")
+        @collection << item
+        (@collection << item).should eql item
+      end # it
+    end # context
+  end # describe
+
+
+
 
   describe "#add!" do
     context "when the argument doesn't respond to :name" do
@@ -96,6 +106,9 @@ describe MQ::Collection do
       (@collection.add! item).should eql(item)
     end # it
   end # describe
+
+
+
 
 
   describe "#<<" do
