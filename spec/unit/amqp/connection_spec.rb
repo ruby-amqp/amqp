d85bec0c596b3ebebb97da29692122291a52bcba
Michael S. Klishin <michael@novemberain.com>
2011-01-19 06:01:47 +0300

    Make sure to reset AMQP state in em_before block, too

--- spec/unit/amqp/connection_spec.rb
+++ spec/unit/amqp/connection_spec.rb
@@ -69,20 +69,30 @@ describe AMQP, 'class object' do
 
 
   describe '.start' do
-    context 'inside EM loop' do
-      include AMQP::SpecHelper
-      em_after { AMQP.cleanup_state }
 
-      it 'yields to given block AFTER connection is established' do
-        em do
-          AMQP.start AMQP_OPTS do
-            @block_fired = true
-            AMQP.connection.should be_connected
-          end
-          done(0.1) { @block_fired.should be_true }
+    #
+    # Environment
+    #
+
+    include AMQP::SpecHelper
+
+    em_before { AMQP.cleanup_state }
+    em_after  { AMQP.cleanup_state }
+
+    #
+    # Examples
+    #
+
+    it 'yields to given block AFTER connection is established' do
+      em do
+        AMQP.start AMQP_OPTS do
+          @block_fired = true
+
+          AMQP.connection.should be_connected
         end
+        done(0.1) { @block_fired.should be_true }
       end
-    end # context 'inside EM loop'
+    end
   end # .start
 
 
