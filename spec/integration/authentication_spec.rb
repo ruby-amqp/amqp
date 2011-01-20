2dfac3a68459b1caa2c8c7a784b1f066ba8fff75
Michael S. Klishin <michael@novemberain.com>
2011-01-20 08:43:14 +0300

    Spec coverage for connection with connection URI

--- spec/integration/authentication_spec.rb
+++ spec/integration/authentication_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe "Authentication attempt with default connection parameters" do
+describe "Authentication attempt" do
 
   #
   # Environment
@@ -12,53 +12,21 @@ describe "Authentication attempt with default connection parameters" do
   em_before { AMQP.cleanup_state }
   em_after  { AMQP.cleanup_state }
 
+  describe "with default connection parameters" do
 
-  #
-  # Examples
-  #
+    #
+    # Examples
+    #
 
-  # assuming there is an account guest with password of "guest" that has
-  # access to / (default vhost)
-  context "when guest/guest has access to /" do
-    after :all do
-      done
-    end
+    # assuming there is an account guest with password of "guest" that has
+    # access to / (default vhost)
+    context "when guest/guest has access to /" do
+      after :all do
+        done
+      end
 
-    it "succeeds" do
-      connection = AMQP.connect
-
-      done(0.3) {
-        connection.should be_connected
-        connection.close
-      }
-    end # it
-  end # context
-end # describe
-
-
-describe "Authentication attempt with explicitly given connection parameters" do
-
-  #
-  # Environment
-  #
-
-  include AMQP::Spec
-
-
-  #
-  # Examples
-  #
-
-  # assuming there is an account amqp_gem with password of "amqp_gem_password" that has
-  # access to /amqp_gem_testbed
-  context "when amqp_gem/amqp_gem_testbed has access to /amqp_gem_testbed" do
-    after :all do
-      done
-    end
-
-    context "and provided credentials are correct" do
       it "succeeds" do
-        connection = AMQP.connect :user => "amqp_gem", :pass => "amqp_gem_password", :vhost => "/amqp_gem_testbed"
+        connection = AMQP.connect
 
         done(0.3) {
           connection.should be_connected
@@ -66,21 +34,80 @@ describe "Authentication attempt with explicitly given connection parameters" do
         }
       end # it
     end # context
-
-    context "and provided credentials ARE INCORRECT" do
-      it "succeeds" do
-        connection = AMQP.connect :user => "amqp_gem", :pass => Time.now.to_i.to_s, :vhost => "/amqp_gem_testbed"
-
-        done(0.5) {
-          connection.should_not be_connected
-        }
-      end # it
-    end
-
-  end # context
-end
+  end # describe
 
 
-describe "Authentication attempt with connection string" do
-end
+  describe "with explicitly given connection parameters" do
 
+    #
+    # Examples
+    #
+
+    # assuming there is an account amqp_gem with password of "amqp_gem_password" that has
+    # access to /amqp_gem_testbed
+    context "when amqp_gem/amqp_gem_testbed has access to /amqp_gem_testbed" do
+      after :all do
+        done
+      end
+
+      context "and provided credentials are correct" do
+        it "succeeds" do
+          connection = AMQP.connect :user => "amqp_gem", :pass => "amqp_gem_password", :vhost => "/amqp_gem_testbed"
+
+          done(0.3) {
+            connection.should be_connected
+            connection.close
+          }
+        end # it
+      end # context
+
+      context "and provided credentials ARE INCORRECT" do
+        it "succeeds" do
+          connection = AMQP.connect :user => "amqp_gem", :pass => Time.now.to_i.to_s, :vhost => "/amqp_gem_testbed"
+
+          done(0.5) {
+            connection.should_not be_connected
+          }
+        end # it
+      end
+    end # context
+  end
+
+
+  describe "with connection string" do
+
+    #
+    # Examples
+    #
+
+    # assuming there is an account amqp_gem with password of "amqp_gem_password" that has
+    # access to /amqp_gem_testbed
+    context "when amqp_gem/amqp_gem_testbed has access to /amqp_gem_testbed" do
+      after :all do
+        done
+      end
+
+      context "and provided credentials are correct" do
+        it "succeeds" do
+          connection = AMQP.connect "amqp://amqp_gem:amqp_gem_password@localhost/amqp_gem_testbed"
+
+          done(0.3) {
+            connection.should be_connected
+            connection.close
+          }
+        end # it
+      end # context
+
+      context "and provided credentials ARE INCORRECT" do
+        it "succeeds" do
+          connection = AMQP.connect "amqp://amqp_gem:#{Time.now.to_i}@localhost/amqp_gem_testbed"
+
+          done(0.5) {
+            connection.should_not be_connected
+            connection.close
+          }
+        end # it
+      end # context
+    end # context
+  end # describe
+end # describe "Authentication attempt"
