33e09d8a19cc68223374d4f9bcf0c18a356b2f07
Michael S. Klishin <michael@novemberain.com>
2011-01-22 20:54:46 +0300

    Enforce UTF-8 when running on Ruby 1.9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,8 +26,12 @@ else
   AMQP_OPTS = {:host => 'localhost', :port => 5672}
 end
 
+#
+# Ruby version-specific
+#
 
-if RUBY_VERSION == "1.8.7"
+case RUBY_VERSION
+when "1.8.7" then
   module ArrayExtensions
     def sample
       self.choice
@@ -37,6 +41,14 @@ if RUBY_VERSION == "1.8.7"
   class Array
     include ArrayExtensions
   end
+when "1.8.6" then
+  raise "Ruby 1.8.6 is not supported. Sorry, pal. Time to move on beyond One True Ruby. Yes, time flies by."
+when /^1.9/ then
+  puts "Encoding.default_internal was #{Encoding.default_internal || 'not set'}, switching to #{Encoding::UTF_8}"
+  Encoding.default_internal = Encoding::UTF_8
+
+  puts "Encoding.default_external was #{Encoding.default_internal || 'not set'}, switching to #{Encoding::UTF_8}"
+  Encoding.default_external = Encoding::UTF_8
 end
 
 
