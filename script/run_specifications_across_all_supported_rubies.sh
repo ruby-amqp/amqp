f307c714557fa1b03855ce062791551c0f81651d
Michael S. Klishin <michael@novemberain.com>
2011-01-25 00:13:30 +0300

    Update multi-Ruby test script

--- script/run_specifications_across_all_supported_rubies.sh
+++ script/run_specifications_across_all_supported_rubies.sh
@@ -1,30 +1,37 @@
 #!/bin/sh
 
-rvm 1.8.7-p174@amqp_gem
-echo "\n\n\nNow running on 1.8.7-p174\n\n\n"
+rvm use 1.8.7-p174@amqp_gem
+ruby --version
+echo "Now running on 1.8.7-p174\n\n\n"
 
+bundle install
 rspec spec
 # to let broker chew through all the messages
 # it may still be processing
 sleep 2
 
 
+rvm use 1.9.1-p378@amqp_gem
+ruby --version
+echo "Now running on 1.9.1-p378\n\n\n"
 
-rvm 1.9.1-p378@amqp_gem
-echo "\n\n\nNow running on 1.9.1-p378\n\n\n"
-
+bundle install
 rspec spec
 sleep 2
 
 
-rvm 1.9.2-p136@amqp_gem
-echo "\n\n\nNow running on 1.9.2-p136\n\n\n"
+rvm use 1.9.2-p136@amqp_gem
+ruby --version
+echo "Now running on 1.9.2-p136\n\n\n"
 
+bundle install
 rspec spec
 sleep 2
 
 
-rvm rbx-head@amqp_gem
-echo "\n\n\nNow running on Rubinius HEAD\n\n\n"
+rvm use rbx-head@amqp_gem
+ruby --version
+echo "Now running on Rubinius HEAD\n\n\n"
 
+bundle install
 rspec spec
