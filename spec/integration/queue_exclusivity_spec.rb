d9cc5cc299b1a35426bef4926c19e1618e575dca
Michael S. Klishin <michael@novemberain.com>
2011-01-24 23:07:53 +0300

    Add tests related to queue exclusivity (read on)
    According to AMQP 0.9.1 spec,
    
      "Exclusive queues may only be accessed by the current connection,
       and are deleted when that connection closes. Passive declaration
       of an exclusive queue by other connections are not allowed."
    
    So far we only cover #1 clause out of 3, because it seems to be the most
    important aspect.
    
    Because attempts to access exclusive queue from another connection
    results in a connection-level exception, the best we can do is to verify
    that connection is closed by the peer. If you have better ideas about
    this specific test case, let us know at http://bit.ly/hjfJrY

---
+++ spec/integration/queue_exclusivity_spec.rb
@@ -0,0 +1,95 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe "Non-exclusive queue" do
+
+  #
+  # Environment
+  #
+
+  include AMQP::Spec
+
+  default_timeout 5
+
+  amqp_before do
+    @connection1 = AMQP.connect
+    @connection2 = AMQP.connect
+  end
+
+
+
+  #
+  # Examples
+  #
+
+  it "can be used across multiple connections" do
+    @connection1.should_not == @connection2
+
+    channel1 = AMQP::Channel.new(@connection1)
+    channel2 = AMQP::Channel.new(@connection2)
+
+    instance1 = AMQP::Queue.new(channel1, "amqpgem.integration.queues.non-exclusive", :exclusive => false, :auto_delete => true)
+    instance2 = AMQP::Queue.new(channel2, "amqpgem.integration.queues.non-exclusive", :exclusive => false, :auto_delete => true)
+
+    exchange1 = channel1.fanout("amqpgem.integration.exchanges.fanout1", :auto_delete => true)
+    exchange2 = channel2.fanout("amqpgem.integration.exchanges.fanout2", :auto_delete => true)
+
+
+    instance1.bind(exchange1).subscribe do |payload|
+    end
+
+    instance2.bind(exchange2).subscribe do |payload|
+    end
+
+    done(0.2) {
+      channel1.should be_open
+      channel1.close
+
+      channel2.should be_open
+      channel2.close
+    }
+  end
+end
+
+
+
+describe "Exclusive queue" do
+
+  #
+  # Environment
+  #
+
+  include AMQP::Spec
+
+  default_timeout 1
+
+  amqp_before do
+    @connection1 = AMQP.connect
+    @connection2 = AMQP.connect
+  end
+
+
+  #
+  # Examples
+  #
+
+  it "can ONLY be used by ONE connection" do
+    @connection1.should_not == @connection2
+
+    channel1 = AMQP::Channel.new(@connection1)
+    channel2 = AMQP::Channel.new(@connection2)
+
+    AMQP::Queue.new(channel1, "amqpgem.integration.queues.exclusive", :exclusive => true)
+    sleep 0.1
+
+    AMQP::Queue.new(channel2, "amqpgem.integration.queues.exclusive", :exclusive => true)
+    
+
+    done(0.2) {
+      channel1.should_not be_closed
+      # because it is a channel-level exception
+      channel2.should be_closed
+    }
+  end
+end
