f196eca4b0f028222788d3fb6921b9d29a137d5d
Jakub Šťastný aka Botanicus <stastny@101ideas.cz>
2011-02-15 18:06:30 +0000

    Yeah, pulling for the SCM might be pretty useful before the build ...

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -3,4 +3,5 @@
 # Server-specific, I have RVM setup at the profile file:
 source /etc/profile
 
+git fetch && git reset origin/master --hard
 rvm 1.9.2-head,1.8.7 exec bundle install --local && rspec spec
