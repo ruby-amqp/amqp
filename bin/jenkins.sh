4e6a10b797334be830fd2162475ae22357f76096
Jakub Šťastný aka Botanicus <stastny@101ideas.cz>
2011-02-15 19:07:54 +0000

    Updated jenkins.sh.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# Server-specific, I have RVM setup at the profile file:
-source /etc/profile
-
+echo -e "\n\n==== Setup ===="
 git fetch && git reset origin/master --hard
-rvm 1.9.2-head,1.8.7 exec bundle install --local && rspec spec
+
+echo -e "\n\n==== Ruby 1.9.2 Head ===="
+rvm 1.9.2-head exec bundle install --local; echo
+rvm 1.9.2-head exec rspec spec
+return_status=$?
+
+echo -e "\n\n==== Ruby 1.8.7 ===="
+rvm 1.8.7 exec bundle install --local; echo
+rvm 1.8.7 exec rspec spec
+return_status=$(expr $return_status + $?)
+
+test $return_status -eq 0
