45dca11c20aa0422208cf3654703fe8a93d48d38
Jakub Šťastný aka Botanicus <stastny@101ideas.cz>
2011-02-16 11:39:45 +0000

    Use bundle exec in jenkins.sh.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -5,12 +5,12 @@ git fetch && git reset origin/master --hard
 
 echo -e "\n\n==== Ruby 1.9.2 Head ===="
 rvm 1.9.2-head exec bundle install --local; echo
-rvm 1.9.2-head exec rspec spec
+rvm 1.9.2-head exec bundle exec rspec spec
 return_status=$?
 
 echo -e "\n\n==== Ruby 1.8.7 ===="
 rvm 1.8.7 exec bundle install --local; echo
-rvm 1.8.7 exec rspec spec
+rvm 1.8.7 exec bundle exec rspec spec
 return_status=$(expr $return_status + $?)
 
 test $return_status -eq 0
