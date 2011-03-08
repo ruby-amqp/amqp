ef1a358fa8727913070626c496752af7d9335fda
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-03-08 03:00:39 +0000

    [CI] Install bundler, source /etc/profile.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 echo -e "\n\n==== Setup ===="
+source /etc/profile
 git fetch && git reset origin/master --hard
 ./bin/set_test_suite_realms_up.sh
 
 echo -e "\n\n==== Ruby 1.9.2 Head ===="
 rvm use 1.9.2-head@ruby-amqp
+gem install bundler --no-ri --no-rdoc
 bundle install --local; echo
 bundle exec rspec spec
 return_status=$?
 
 echo -e "\n\n==== Ruby 1.8.7 ===="
 rvm use 1.8.7@ruby-amqp
+gem install bundler --no-ri --no-rdoc
 bundle install --local; echo
 bundle exec rspec spec
 return_status=$(expr $return_status + $?)
