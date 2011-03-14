540490e82e40471406add4403060c51c2797c25e
Michael S. Klishin <michael@novemberain.com>
2011-03-14 19:35:03 +0300

    Outline of public AMQP and AMQP::Client methods we want to support in 0.8

---
+++ lib/amqp/client.rb
@@ -0,0 +1,54 @@
+# encoding: utf-8
+
+module AMQP
+  # @api public
+  def self.client
+    # TODO
+  end
+
+  # @api public
+  def self.connect(arg = nil)
+    # TODO
+  end
+
+  # def self.client=(value)
+  # end
+
+  module Client
+    # @api public
+    def initialize(options = {})
+      # TODO
+    end # initialize(options = {})
+
+
+    # @api public
+    def tcp_connection_established?
+      # TODO
+    end
+
+    # @api public
+    def connected?
+      # TODO
+    end
+
+    # @api public
+    def channels
+      # TODO
+    end # channels
+
+    # @api public
+    def send(data, options = {})
+      # TODO
+    end # send(data, options = {})
+
+    # @api public
+    def closing?
+      # TODO
+    end # closing?
+
+    # @api public
+    def reconnect(force = false)
+      # TODO
+    end # reconnect(force = false)
+  end # Client
+end # AMQP
