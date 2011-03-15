da58fa6bd19e92498812cf3d54fc8ceb48b93271
Michael S. Klishin <michael@novemberain.com>
2011-03-15 22:01:45 +0300

    Towards more working examples

--- examples/ack.rb
+++ examples/ack.rb
@@ -5,18 +5,21 @@ require 'amqp'
 
 # For ack to work appropriately you must shutdown AMQP gracefully,
 # otherwise all items in your queue will be returned
-Signal.trap('INT') { AMQP.stop { EM.stop } }
-Signal.trap('TERM') { AMQP.stop { EM.stop } }
+Signal.trap('INT')  { AMQP.stop { puts "About to stop EM reactor"; EM.stop } }
+Signal.trap('TERM') { AMQP.stop { puts "About to stop EM reactor"; EM.stop } }
 
-AMQP.start(:host => 'localhost') do
-  AMQP::Channel.queue('awesome').publish('Totally rad 1')
-  AMQP::Channel.queue('awesome').publish('Totally rad 2')
-  AMQP::Channel.queue('awesome').publish('Totally rad 3')
+AMQP.start(:host => 'localhost') do |connection|
+  puts "Connected!"
+  channel = AMQP::Channel.new(connection)
+
+  channel.queue('awesome').publish('Totally rad 1')
+  channel.queue('awesome').publish('Totally rad 2')
+  channel.queue('awesome').publish('Totally rad 3')
 
   i = 0
 
   # Stopping after the second item was acked will keep the 3rd item in the queue
-  AMQP::Channel.queue('awesome').subscribe(:ack => true) do |h, m|
+  channel.queue('awesome').subscribe(:ack => true) do |h, m|
     if (i += 1) == 3
       puts 'Shutting down...'
       AMQP.stop { EM.stop }
@@ -30,18 +33,3 @@ AMQP.start(:host => 'localhost') do
     end
   end
 end
-
-__END__
-
-Totally rad 1
-Totally rad 2
-Shutting down...
-Totally rad 3 (ignored, redelivered later)
-
-When restarted:
-
-Totally rad 3
-Totally rad 1
-Shutting down...
-Totally rad 2 (ignored, redelivered later)
-Totally rad 3 (ignored, redelivered later)
--- examples/pingpong.rb
+++ examples/pingpong.rb
@@ -4,13 +4,16 @@ $:.unshift(File.expand_path("../../lib", __FILE__))
 require 'amqp'
 
 AMQP.start(:host => 'localhost') do |connection|
-
-  # Send Connection.Close on Ctrl+C
-  trap(:INT) do
-    unless connection.closing?
-      connection.close { exit! }
-    end
+  show_stopper = Proc.new do
+    $stdout.puts "Stopping..."
+    # now change this to just EM.stop and it
+    # unbinds instantly
+    connection.close {
+      EM.stop { exit }
+    }
   end
+  Signal.trap "INT", &show_stopper
+
 
   def log(*args)
     p [ Time.now, *args ]
@@ -18,24 +21,25 @@ AMQP.start(:host => 'localhost') do |connection|
 
   # AMQP.logging = true
 
-  amq = AMQP::Channel.new(connection)
-  q1  = amq.queue('one')
-  q2  = amq.queue('two')
+  amq      = AMQP::Channel.new(connection)
+  exchange = AMQP::Exchange.default(amq)
+  q1       = amq.queue('one')
+  q2       = amq.queue('two')
 
   EM.add_periodic_timer(1) {
     puts
 
     log :sending, 'ping'
+    exchange.publish('ping')
+  }
+
+  100.times do
     q1.publish('ping')
-  }
+  end
 
-  q1.subscribe { |msg|
+  q1.subscribe do |msg|
     log 'one', :received, msg, :sending, 'pong'
-    q2.publish('pong')
-  }
-
-  #q2.subscribe { |msg|
-  #  log 'two', :received, msg
-  #}
-
+    exchange.publish('pong')
+  end
+  q2.subscribe { |msg| log('two', :received, msg) }
 end
