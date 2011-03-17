e8fd6fc088417bb4b11a9bb5778ef9b2637ea12e
Michael S. Klishin <michael@novemberain.com>
2011-03-17 20:00:33 +0300

    Do not define top-level BlankSlate to avoid conflicts with Builder 2.x
    We do not need it to be top-level anyway. See http://bit.ly/fe9VPp for more
    information. Kudos to John Axel Eriksson for reporting this tricky issue
    that is both Ruby 1.9.2 and Builder 2.1.2-specific.

--- lib/amqp/connection.rb
+++ lib/amqp/connection.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 
 require "amqp/ext/em"
-require "amqp/ext/blankslate"
 
 require "amqp/client"
 
--- lib/amqp/ext/blankslate.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-unless defined?(BlankSlate)
-  class BlankSlate < BasicObject; end if defined?(BasicObject)
-
-  class BlankSlate #:nodoc:
-    instance_methods.each { |m| undef_method m unless m =~ /^__/ }
-  end
-end
