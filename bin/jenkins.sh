b7fc52ae194186b9efbebb083505b1f8c50586c5
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-03-29 17:59:13 +0100

    Use bundle update, not bundle install, that's why CI keeps failing every time after a while.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -11,14 +11,14 @@ echo "~ NOT running ./bin/set_test_suite_realms_up.sh"
 echo -e "\n\n==== Ruby 1.9.2 Head ===="
 rvm use 1.9.2-head@ruby-amqp
 gem install bundler --no-ri --no-rdoc
-bundle install --path vendor/bundle/1.9.2 --without development; echo
+bundle update --path vendor/bundle/1.9.2 --without development; echo
 bundle exec rspec spec
 return_status=$?
 
 echo -e "\n\n==== Ruby 1.8.7 ===="
 rvm use 1.8.7@ruby-amqp
 gem install bundler --no-ri --no-rdoc
-bundle install --path vendor/bundle/1.8.7 --without development; echo
+bundle update --path vendor/bundle/1.8.7 --without development; echo
 bundle exec rspec spec
 return_status=$(expr $return_status + $?)
 
