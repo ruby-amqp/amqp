80bb5805d1f3831bd9ac1c699cbd34e230c76e5c
Michael S. Klishin <michael@novemberain.com>
2011-04-13 10:47:03 +0400

    Documentation update for AMQP::Client and AMQP::BasicClient. We have more clients than some enterprises.

--- lib/amqp/basic_client.rb
+++ lib/amqp/basic_client.rb
@@ -1,18 +1,22 @@
 require "amq/client/adapters/event_machine"
 
 module AMQP
+  # AMQP client implementation based on amq-client library. Left here for API compatibility
+  # with 0.7.x series.
+  #
+  # @note This class is not part of the public API and may be removed in the future without any warning.
   class BasicClient < AMQ::Client::EventMachineClient
 
     #
     # API
     #
 
-    # @api public
+    # @api plugin
     def connected?
       self.opened?
     end
 
-    # @api public
+    # @api plugin
     def reconnect(force = false)
       # TODO
       raise NotImplementedError.new
--- lib/amqp/client.rb
+++ lib/amqp/client.rb
@@ -9,7 +9,14 @@ module AMQP
     AMQPS      = "amqps".freeze
 
 
-    # @api public
+    # {AMQP.connect} delegates to this method. There is no reason for applications or
+    # libraries to use this method directly.
+    #
+    #
+    # @note This method is not part of the public API and may be removed in the future without any warning.
+    # @see AMQP.start
+    # @see AMQP.connect
+    # @api plugin
     def self.connect(connection_string_or_options = {}, options = {}, &block)
       opts = case connection_string_or_options
              when String then
@@ -27,7 +34,10 @@ module AMQP
       end
     end
 
-    # @return [Hash] Connection parameters (:user, :pass, :vhost, :host, :port, :ssl)
+    # Parses AMQP connection string and returns it's components as a hash.
+    #
+    # @param [String] connection_string AMQP connection URI, à la JDBC connection string. For example: amqp://bus.megacorp.internal:5877/qa
+    # @return [Hash] Connection parameters (:username, :password, :vhost, :host, :port, :ssl)
     # @api public
     def self.parse_connection_uri(connection_string)
       uri = URI.parse(connection_string)
@@ -48,12 +58,12 @@ module AMQP
 
 
 
-  # @api public
+  # @private
   def self.client
     @client_implementation ||= BasicClient
   end
 
-  # @api public
+  # @private
   def self.client=(value)
     @client_implementation = value
   end
