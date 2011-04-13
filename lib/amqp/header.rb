58c45c9d5358c4af7714ef94a69c9ee5183a8baf
Michael S. Klishin <michael@novemberain.com>
2011-04-13 10:55:08 +0400

    Documentation update for AMQP::Header

--- lib/amqp/header.rb
+++ lib/amqp/header.rb
@@ -1,13 +1,23 @@
 # encoding: utf-8
 
 module AMQP
+  # We keep this class around for sake of API compatibility with 0.7.x series.
+  #
+  # @note This class is not part of the public API and may be removed in the future without any warning.
   class Header
 
     #
     # API
     #
 
-    attr_reader :channel, :method, :header
+    # @return [AMQP::Channel]
+    attr_reader :channel
+    # AMQP method frame this header is associated with. Carries additional information that varies between AMQP methods.
+    # @return [AMQ::Protocol::Method]
+    attr_reader :method
+    # AMQP message header as a hash
+    # @return [Hash]
+    attr_reader :header
 
     # @api public
     def initialize(channel, method, header)
@@ -15,13 +25,14 @@ module AMQP
     end
 
     # Acknowledges the receipt of this message with the server.
+    # @param [Boolean] multiple Whether or not to acknowledge multiple messages
     # @api public
     def ack(multiple = false)
       @channel.acknowledge(@method.delivery_tag, multiple)
     end
 
     # Reject this message.
-    # * :requeue => true | false (default false)
+    # @option opts [Hash] :requeue (false) Whether message should be requeued.
     # @api public
     def reject(opts = {})
       @channel.reject(@method.delivery_tag, opts.fetch(:requeue, false))
