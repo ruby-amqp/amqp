05ce7a77de9dde749c72799ce22329107533cd46
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-04-13 07:47:50 +0100

    Added note about rpc gem.

--- lib/amqp/rpc.rb
+++ lib/amqp/rpc.rb
@@ -12,6 +12,7 @@ please use
 
 
 Both amqp/rpc.rb and AMQP::RPC implementation will be REMOVED before 1.0 release.
+If you need RPC, we recommend the rpc gem (see https://github.com/ruby-amqp/rpc).
 MESSAGE
 
 require "amqp/deprecated/rpc"
