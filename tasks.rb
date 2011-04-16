8f6de92f6f04f072bdaff3e6fef2f3506bb88a5c
Michael S. Klishin <michael@novemberain.com>
2011-04-16 09:41:14 +0400

    require contributors, handle encoding on 1.9.2

--- tasks.rb
+++ tasks.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/env nake
 # encoding: utf-8
 
+if RUBY_VERSION =~ /^1.9/
+  Encoding.default_internal = Encoding::UTF_8
+  Encoding.default_external = Encoding::UTF_8
+end
+
+
+require "contributors"
 load "contributors.nake"
