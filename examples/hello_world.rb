35e517312903c2574df9e591cd547f88c85c9de6
Michael S. Klishin <michael@novemberain.com>
2011-04-19 04:35:39 +0400

    Update Hello world example to be 0.7.x compatible. Clarify that not all examples are.

--- examples/hello_world.rb
+++ examples/hello_world.rb
@@ -9,22 +9,20 @@ $:.unshift(File.expand_path("../../lib", __FILE__))
 require 'amqp'
 
 EventMachine.run do
-  EventMachine.run do
-    connection = AMQP.connect(:host => '127.0.0.1')
-    puts "Connected to AMQP broker. Running #{AMQP::VERSION} version of the gem..."
+  connection = AMQP.connect(:host => '127.0.0.1')
+  puts "Connected to AMQP broker. Running #{AMQP::VERSION} version of the gem..."
 
-    channel  = AMQP::Channel.new(connection)
-    queue    = channel.queue("amqpgem.examples.hello_world", :auto_delete => true)
-    exchange = channel.direct("")
+  channel  = AMQP::Channel.new(connection)
+  queue    = channel.queue("amqpgem.examples.hello_world", :auto_delete => true)
+  exchange = channel.direct("")
 
-    queue.subscribe do |payload|
-      puts "Received a message: #{payload}. Disconnecting..."
+  queue.subscribe do |payload|
+    puts "Received a message: #{payload}. Disconnecting..."
 
-      connection.close {
-        EM.stop { exit }
-      }
-    end
-
-    exchange.publish "Hello, world!", :routing_key => queue.name
+    connection.close {
+      EM.stop { exit }
+    }
   end
+
+  exchange.publish "Hello, world!", :routing_key => queue.name
 end
