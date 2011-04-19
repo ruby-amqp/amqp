a7fee7b9b54955d22cae36aaba2d8e001b71f82b
Michael S. Klishin <michael@novemberain.com>
2011-04-19 13:58:42 +0400

    More Hello World examples: this time with EventMachine loop running in a separate thread

---
+++ examples/hello_world_with_eventmachine_in_a_separate_thread.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bundler"
+Bundler.setup
+
+$:.unshift(File.expand_path("../../lib", __FILE__))
+
+require 'amqp'
+
+t = Thread.new { EventMachine.run }
+
+EventMachine.next_tick {
+  connection = AMQP.connect(:host => '127.0.0.1')
+  puts "Connected to AMQP broker. Running #{AMQP::VERSION} version of the gem..."
+
+  channel  = AMQP::Channel.new(connection)
+  queue    = channel.queue("amqpgem.examples.hello_world", :auto_delete => true)
+  exchange = channel.direct("")
+
+  queue.subscribe do |payload|
+    puts "Received a message: #{payload}. Disconnecting..."
+
+    connection.close {
+      EM.stop { exit }
+    }
+  end
+
+  exchange.publish "Hello, world!", :routing_key => queue.name  
+}
+
+t.join
