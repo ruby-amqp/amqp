d4ee3d26f31c80743b935b89b39e8ea1d283538b
Michael S. Klishin <michael@novemberain.com>
2011-04-22 23:36:11 +0400

    AMQP::BasicClient is likely to stay with us for a long time (for backwards-compatibility reasons)

--- lib/amqp/basic_client.rb
+++ lib/amqp/basic_client.rb
@@ -3,10 +3,6 @@
 require "amq/client/adapters/event_machine"
 
 module AMQP
-  # AMQP client implementation based on amq-client library. Left here for API compatibility
-  # with 0.7.x series.
-  #
-  # @note This class is not part of the public API and may be removed in the future without any warning.
   class BasicClient < AMQ::Client::EventMachineClient
 
     #
