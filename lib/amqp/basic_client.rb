c0f2c147127021203045ba9ce6e7adb5d5060390
Michael S. Klishin <michael@novemberain.com>
2011-04-22 23:32:41 +0400

    amq-client now has the same order of parameters

--- lib/amqp/basic_client.rb
+++ lib/amqp/basic_client.rb
@@ -18,9 +18,16 @@ module AMQP
       self.opened?
     end
 
+    # Reconnect to the broker using current connection settings.
+    #
+    # @param [Boolean] force Enforce immediate connection
+    # @param [Fixnum] period If given, reconnection will be delayed by this period, in seconds.
     # @api plugin
     def reconnect(force = false, period = 2)
-      super(period, force)
+      # we do this to make sure this method shows up in our documentation
+      # this method is too important to leave out and YARD currently does not
+      # support cross-referencing to dependencies. MK.
+      super(force, period)
     end # reconnect(force = false)
   end
 end
