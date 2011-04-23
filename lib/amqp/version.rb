80159ed00b5e5aac5d37661dee5b118374bf86dc
Michael S. Klishin <michael@novemberain.com>
2011-04-23 21:55:47 +0400

    Clarify that AMQP::VERSION is the gem version, not AMQP protocol version it implements

--- lib/amqp/version.rb
+++ lib/amqp/version.rb
@@ -1,5 +1,10 @@
 # encoding: utf-8
 
 module AMQP
+  # AMQP gem version. Not to be confused with AMQP protocol version
+  # it implements. For that, see {AMQ::Protocol::VERSION}
+  #
+  # @see AMQ::Protocol::VERSION
+  # @return [String] AMQP gem version
   VERSION = '0.8.0.rc2'
 end
