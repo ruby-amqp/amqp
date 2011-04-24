1f476e7967355fa51f2cc8216571ae7adf3244ec
Michael S. Klishin <michael@novemberain.com>
2011-04-24 19:37:55 +0400

    Add example that immediately binds a server-named queue. A crazy edge case 0.7.x handles well.

---
+++ examples/immediately_bind_a_server_named_queue.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bundler"
+Bundler.setup
+
+$:.unshift(File.expand_path("../../lib", __FILE__))
+
+require 'amqp'
+
+
+puts "=> AMQP gem #{AMQP::VERSION}"
+puts
+AMQP.start do |connection, open_ok|
+  channel = AMQP::Channel.new
+  puts "Channel ##{channel.channel} is now open!"
+
+  xchange = channel.fanout("amq.fanout")
+  q = AMQP::Queue.new(channel, "", :auto_delete => true).bind(xchange).subscribe do |header, payload|
+    puts "Got a payload: #{payload}"
+  end
+
+  EventMachine.add_periodic_timer(0.5) {
+    xchange.publish("à bientôt!", :key => q.name)
+  }
+
+
+  show_stopper = Proc.new do
+    $stdout.puts "Stopping..."
+
+    connection.close {
+      EM.stop { exit }
+    }
+  end
+
+  Signal.trap "INT", show_stopper
+  EM.add_timer(3, show_stopper)
+end
