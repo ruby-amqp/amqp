42ee8b45f4016c11bf666582cd1c7722ca8d4a40
Michael S. Klishin <michael@novemberain.com>
2011-04-24 23:01:57 +0400

    Turn example of an immediately bound server-named queue into a spec example

---
+++ spec/integration/declare_and_immediately_bind_a_server_named_queue_spec.rb
@@ -0,0 +1,42 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe "Server-named", AMQP::Queue do
+
+  #
+  # Environment
+  #
+
+  include EventedSpec::AMQPSpec
+
+  default_timeout 5
+
+  amqp_before do
+    @channel = AMQP::Channel.new
+  end
+
+
+  #
+  # Examples
+  #
+
+
+  it "delays binding until after queue.declare-ok arrives" do
+    mailbox  = []
+    exchange = @channel.fanout("amq.fanout")
+    input    = "Independencia de resolución, ¿una realidad en Mac OS X Lion?"
+
+    @channel.queue("", :auto_delete => true).bind(exchange).subscribe do |header, body|
+      mailbox << body
+    end
+
+    delayed(0.3) {
+      exchange.publish(input)
+    }
+
+    done(0.5) {
+      mailbox.should include(input)
+    }
+  end
+end
