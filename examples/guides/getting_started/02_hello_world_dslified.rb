d6c0c8709eaaf98543f83d63761e6a5fd264520e
Michael S. Klishin <michael@novemberain.com>
2011-04-26 03:43:01 +0400

    Initial bits of Getting Started guide

---
+++ examples/guides/getting_started/02_hello_world_dslified.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "rubygems"
+require "amqp"
+
+EventMachine.run do
+  AMQP.connect(:host => '127.0.0.1') do |connection|
+    puts "Connected to AMQP broker. Running #{AMQP::VERSION} version of the gem..."
+
+    channel  = AMQP::Channel.new(connection)
+
+    channel.queue("amqpgem.examples.helloworld", :auto_delete => true).subscribe do |payload|
+      puts "Received a message: #{payload}. Disconnecting..."
+
+      connection.close {
+        EM.stop { exit }
+      }
+    end
+
+    channel.direct("").publish "Hello, world!", :routing_key => queue.name
+  end
+end
