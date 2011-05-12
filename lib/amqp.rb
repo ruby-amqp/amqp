059e5a884a6530a3ca6300b93d84aebe8aa14f53
Michael S. Klishin <michael@novemberain.com>
2011-05-12 11:47:27 +0400

    When on 1.8.7-p249, fail early, fast, loudly and loudly with a clear explanation of possible solution options

--- lib/amqp.rb
+++ lib/amqp.rb
@@ -4,6 +4,9 @@ require "amq/client"
 require "amq/client/adapters/event_machine"
 
 require "amqp/version"
+if RUBY_VERSION =~ /^1.8.7/
+  require "amqp/compatibility/ruby187_patchlevel_check"
+end
 require "amqp/exceptions"
 require "amqp/connection"
 require "amqp/exchange"
---
+++ lib/amqp/compatibility/ruby187_patchlevel_check.rb
@@ -0,0 +1,17 @@
+if RUBY_VERSION =~ /^1.8.7/
+  require "rbconfig"
+  conf = RbConfig::CONFIG
+
+  # looks like PATCHLEVEL was added after 1.8.7-p249 release :( MK.
+  if (conf["PATCHLEVEL"] && conf["PATCHLEVEL"] == "249") || (conf["libdir"] =~ /1.8.7-p249/)
+    raise <<-MESSAGE
+IMPORTANT: amqp gem 0.8.0+ does not support Ruby 1.8.7-p249 (this exact patch level. p174 and p334 are supported!)
+because of a nasty Ruby bug (http://bit.ly/iONBmH). Refer to http://groups.google.com/group/ruby-amqp/browse_thread/thread/37d6dcc1aea1c102
+for more information, especially if you want to verify whether a particular Ruby patch level or version or build suffers from
+this issue.
+
+To reiterate: 1.8.7-p174 and 1.8.7-p334 are supported. The fix has been committed to MRI in December 2009. It's
+a good idea to upgrade, although downgrading to p174 is an option, too.
+    MESSAGE
+  end
+end
