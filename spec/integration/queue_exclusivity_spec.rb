455f20aac0014f1e87a7f8ce7a05a7fb3ff1c4d3
Michael S. Klishin <michael@novemberain.com>
2011-05-14 02:54:54 +0400

    Satisfy Java EventMachine reactor. Ugh.

--- spec/integration/queue_exclusivity_spec.rb
+++ spec/integration/queue_exclusivity_spec.rb
@@ -86,7 +86,7 @@ describe "Exclusive queue" do
     AMQP::Queue.new(channel2, "amqpgem.integration.queues.exclusive", :exclusive => true)
 
 
-    done(0.2) {
+    done(0.5) {
       channel1.should_not be_closed
       # because it is a channel-level exception
       channel2.should be_closed
