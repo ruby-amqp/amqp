6138c404380fa9bb0b61d752a08ee55c73ae73c9
Michael S. Klishin <michael@novemberain.com>
2011-05-17 23:40:04 +0400

    Demonstrate that when AMQP::Channel#initialize callback arity is 1, we only yield 1 argument

---
+++ examples/channels/open_channel_with_one_arity_callback.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bundler"
+Bundler.setup
+
+$:.unshift(File.expand_path("../../../lib", __FILE__))
+
+require 'amqp'
+
+
+puts "=> Channel#initialize example that uses a block"
+puts
+AMQP.start(:host => 'localhost') do |connection|
+  AMQP::Channel.new do |channel|
+    puts "Channel ##{channel.id} is now open!"
+  end
+
+
+  show_stopper = Proc.new do
+    $stdout.puts "Stopping..."
+
+    connection.close {
+      EM.stop { exit }
+    }
+  end
+
+  Signal.trap "INT", show_stopper
+  EM.add_timer(2, show_stopper)
+end
