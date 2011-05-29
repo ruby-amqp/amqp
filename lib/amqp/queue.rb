376ac81a9d88028ef9dbf25b666b2f9f6533c35d
Michael S. Klishin <michael@novemberain.com>
2011-05-30 00:46:08 +0400

    Document consumer (message subscription) exclusivity option

--- lib/amqp/queue.rb
+++ lib/amqp/queue.rb
@@ -521,9 +521,14 @@ module AMQP
     #                                        method it will raise a channel or connection exception.
     #
     # @option opts [#call] :confirm (nil)   If set, this proc will be called when the server confirms subscription
-    #                                       to the queue with a ConsumeOk message. Setting this option will
+    #                                       to the queue with a basic.consume-ok message. Setting this option will
     #                                       automatically set :nowait => false. This is required for the server
     #                                       to send a confirmation.
+    #
+    # @option opts [Boolean] :exclusive     Request exclusive consumer access, meaning only this consumer can access the queue.
+    #                                       This is useful when you want a long-lived shared queue to be temporarily accessible by just
+    #                                       one application (or thread, or process). If application exclusive consumer is part of crashes
+    #                                       or loses network connection to the broker, channel is closed and exclusive consumer is thus cancelled.
     #
     #
     # @yield [headers, payload] When block only takes one argument, yields payload to it. In case of two arguments, yields headers and payload.
