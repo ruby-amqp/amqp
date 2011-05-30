0ae35930bb54ee3d6caa61a372e4e51a2296d9f3
Michael S. Klishin <michael@novemberain.com>
2011-05-30 05:00:14 +0400

    Update argument names to refer to message metadata as `metadata`

--- examples/publishing/returned_messages.rb
+++ examples/publishing/returned_messages.rb
@@ -22,7 +22,7 @@ AMQP.start(:host => '127.0.0.1') do |connection|
   channel.on_error { |ch, channel_close| EventMachine.stop; raise "channel error: #{channel_close.reply_text}" }
 
   exchange = channel.fanout("amq.fanout")
-  exchange.on_return do |basic_return, header, payload|
+  exchange.on_return do |basic_return, metadata, payload|
     puts "#{payload} was returned! reply_code = #{basic_return.reply_code}, reply_text = #{basic_return.reply_text}"
   end
 
