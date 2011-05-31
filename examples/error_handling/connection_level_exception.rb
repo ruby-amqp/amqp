a6d86046baea156a09280b7f17ade47346b50d90
Michael S. Klishin <michael@novemberain.com>
2011-06-01 02:51:08 +0400

    Add connection-level exception handling example

---
+++ examples/error_handling/connection_level_exception.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bundler"
+Bundler.setup
+
+$:.unshift(File.expand_path("../../../lib", __FILE__))
+
+require 'amqp'
+
+EventMachine.run do
+  AMQP.connect(:host => '127.0.0.1', :port => 5672) do |connection|
+    puts "Connected to AMQP broker. Running #{AMQP::VERSION} version of the gem..."
+
+
+    connection.on_error do |conn, connection_close|
+      puts "Handling a connection-level exception: #{connection_close.reply_text}"
+
+      EventMachine.stop
+    end
+
+    # send_frame is NOT part of the public API, but it is public for entities like AMQ::Client::Channel
+    # and we use it here to trigger a connection-level exception. MK.
+    connection.send_frame(AMQ::Protocol::Connection::TuneOk.encode(1000, 1024 * 128 * 1024, 10))
+  end
+end
