d167e5fd5c36384a36fad51ded03a6b3a3e94417
Michael S. Klishin <michael@novemberain.com>
2011-06-23 06:57:02 +0400

    "Working With Exchanges" documentation guide update

--- examples/routing/round_robin_with_the_default_exchange.rb
+++ examples/routing/round_robin_with_the_default_exchange.rb
@@ -12,22 +12,21 @@ EventMachine.run do
   AMQP.connect do |connection|
     channel1  = AMQP::Channel.new(connection)
     channel2  = AMQP::Channel.new(connection)
-    exchange = channel1.direct("amqpgem.examples.exchanges.direct", :auto_delete => true)
+    exchange = channel1.default_exchange
 
-    q1 = channel1.queue("amqpgem.examples.queues.shared", :auto_delete => true).bind(exchange, :routing_key => "shared.key")
+    q1 = channel1.queue("amqpgem.examples.queues.shared", :auto_delete => true)
     q1.subscribe do |payload|
       puts "Queue #{q1.name} on channel 1 received #{payload}"
     end
 
-    # since the queue is shared, binding here is redundant but we will leave it in for completeness.
-    q2 = channel2.queue("amqpgem.examples.queues.shared", :auto_delete => true).bind(exchange, :routing_key => "shared.key")
+    q2 = channel2.queue("amqpgem.examples.queues.shared", :auto_delete => true)
     q2.subscribe do |payload|
       puts "Queue #{q2.name} on channel 2 received #{payload}"
     end
 
     # Publish some test data in a bit, after all queues are declared & bound
     EventMachine.add_timer(1.2) do
-      5.times { |i| exchange.publish("Hello #{i}, direct exchanges world!", :routing_key => "shared.key") }
+      5.times { |i| exchange.publish("Hello #{i}, direct exchanges world!", :routing_key => "amqpgem.examples.queues.shared") }
     end
 
 
