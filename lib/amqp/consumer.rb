ffc161a378d38643b53d045adc8cb47c0451f023
Michael S. Klishin <michael@novemberain.com>
2011-06-27 10:27:38 +0400

    Make AMQP::Consumer methods appear in API documentation, add initial spec example

--- lib/amqp/consumer.rb
+++ lib/amqp/consumer.rb
@@ -1,6 +1,142 @@
 require "amq/client/async/consumer"
 
 module AMQP
+  # AMQP consumers are entities that handle messages delivered to them ("push API" as opposed to "pull API") by AMQP broker.
+  # Every consumer is associated with a queue. Consumers can be exclusive (no other consumers can be registered for the same queue)
+  # or not (consumers share the queue). In the case of multiple consumers per queue, messages are distributed in round robin
+  # manner with respect to channel-level prefetch setting).
+  #
+  # @see AMQP::Queue
+  # @see AMQP::Queue#subscribe
+  # @see AMQP::Queue#cancel
   class Consumer < AMQ::Client::Async::Consumer
+
+    #
+    # API
+    #
+
+    # @return [AMQP::Channel] Channel this consumer uses
+    attr_reader :channel
+    # @return [AMQP::Queue] Queue messages are consumed from
+    attr_reader :queue
+    # @return [String] Consumer tag, unique consumer identifier
+    attr_reader :consumer_tag
+    # @return [Hash] Custom subscription metadata
+    attr_reader :arguments
+
+    # @return [AMQ::Client::ConsumerTagGenerator] Consumer tag generator
+    def self.tag_generator
+      @tag_generator ||= AMQ::Client::ConsumerTagGenerator.new
+    end # self.tag_generator
+
+    # @param [AMQ::Client::ConsumerTagGenerator] Assigns consumer tag generator that will be used by consumer instances
+    # @return [AMQ::Client::ConsumerTagGenerator] Provided argument
+    def self.tag_generator=(generator)
+      @tag_generator = generator
+    end
+
+
+    def initialize(channel, queue, consumer_tag = self.class.tag_generator.generate_for(queue), exclusive = false, no_ack = false, arguments = {}, no_local = false)
+      super(channel, queue, consumer_tag, exclusive, no_ack, arguments, no_local)
+    end # initialize
+
+    # @return [Boolean] true if this consumer is exclusive (other consumers for the same queue are not allowed)
+    def exclusive?
+      super
+    end # exclusive?
+
+    # Begin consuming messages from the queue
+    # @return [AMQP::Consumer] self
+    def consume(nowait = false, &block)
+      super(nowait, &block)
+    end # consume(nowait = false, &block)
+
+    # Used by automatic recovery code.
+    # @api plugin
+    # @return [AMQP::Consumer] self
+    def resubscribe(&block)
+      super(&block)
+    end # resubscribe(&block)
+
+    # @return [AMQP::Consumer] self
+    def cancel(nowait = false, &block)
+      super(nowait, &block)
+    end # cancel(nowait = false, &block)
+
+
+    # Register a block that will be used to handle delivered messages.
+    #
+    # @return [AMQP::Consumer] self
+    # @see AMQP::Queue#subscribe
+    def on_delivery(&block)
+      super(&block)
+    end # on_delivery(&block)
+
+
+    # @group Acknowledging & Rejecting Messages
+
+    # Acknowledge a delivery tag.
+    # @return [Consumer]  self
+    #
+    # @api public
+    # @see http://bit.ly/htCzCX AMQP 0.9.1 protocol documentation (Section 1.8.3.13.)
+    def acknowledge(delivery_tag)
+      super(delivery_tag)
+    end # acknowledge(delivery_tag)
+
+    #
+    # @return [Consumer]  self
+    #
+    # @api public
+    # @see http://bit.ly/htCzCX AMQP 0.9.1 protocol documentation (Section 1.8.3.14.)
+    def reject(delivery_tag, requeue = true)
+      super(delivery_tag, requeue)
+    end # reject(delivery_tag, requeue = true)
+
+    # @endgroup
+
+
+    # @group Error Handling & Recovery
+
+    # Defines a callback that will be executed after TCP connection is interrupted (typically because of a network failure).
+    # Only one callback can be defined (the one defined last replaces previously added ones).
+    #
+    # @api public
+    def on_connection_interruption(&block)
+      super(&block)
+    end # on_connection_interruption(&block)
+    alias after_connection_interruption on_connection_interruption
+
+
+    # Defines a callback that will be executed after TCP connection is recovered after a network failure
+    # but before AMQP connection is re-opened.
+    # Only one callback can be defined (the one defined last replaces previously added ones).
+    #
+    # @api public
+    def before_recovery(&block)
+      super(&block)
+    end # before_recovery(&block)
+
+    # Defines a callback that will be executed when AMQP connection is recovered after a network failure..
+    # Only one callback can be defined (the one defined last replaces previously added ones).
+    #
+    # @api public
+    def on_recovery(&block)
+      super(&block)
+    end # on_recovery(&block)
+    alias after_recovery on_recovery
+
+
+    # Called by associated connection object when AMQP connection has been re-established
+    # (for example, after a network failure).
+    #
+    # @api plugin
+    def auto_recover
+      super
+    end # auto_recover
+
+    # @endgroup
+
+
   end # Consumer
 end # AMQP
---
+++ spec/integration/multiple_consumers_per_queue_spec.rb
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+require "spec_helper"
+
+describe "Multiple non-exclusive consumers per queue" do
+
+  #
+  # Environment
+  #
+
+  include EventedSpec::AMQPSpec
+  include EventedSpec::SpecHelper
+
+  default_options AMQP_OPTS
+  default_timeout 3
+
+
+  #
+  # Examples
+  #
+
+  context "with equal prefetch levels" do
+    let(:messages) { (0..99).map {|i| "Message #{i}" } }
+
+    it "have messages distributed to them in the round-robin manner" do
+      @consumer1_mailbox = []
+      @consumer2_mailbox = []
+      @consumer3_mailbox = []
+
+      channel = AMQP::Channel.new
+      channel.on_error do |ch, channel_close|
+        raise(channel_close.reply_text)
+      end
+
+      queue   = channel.queue("amqpgem.integration.roundrobin.queue1", :auto_delete => true) do
+        consumer1 = AMQP::Consumer.new(channel, queue)
+        consumer2 = AMQP::Consumer.new(channel, queue)
+        consumer3 = AMQP::Consumer.new(channel, queue, "#{queue.name}-consumer-#{rand}-#{Time.now}", false, true)
+
+        consumer1.consume.on_delivery do |method, header, payload|
+          @consumer1_mailbox << payload
+        end
+
+        consumer2.consume(true).on_delivery do |method, header, payload|
+          @consumer2_mailbox << payload
+        end
+
+        consumer3.consume(false).on_delivery do |method, header, payload|
+          @consumer3_mailbox << payload
+        end
+      end
+
+      exchange = channel.default_exchange
+      exchange.on_return do |basic_return, metadata, payload|
+        raise(basic_return.reply_text)
+      end
+
+      EventMachine.add_timer(1.0) do
+        messages.each do |message|
+          exchange.publish(message, :immediate => true, :mandatory => true, :routing_key => queue.name)
+        end
+      end
+
+      done(1.5) {
+        @consumer1_mailbox.size.should == 34
+        @consumer2_mailbox.size.should == 33
+        @consumer3_mailbox.size.should == 33
+      }
+    end
+  end # it
+end # context
