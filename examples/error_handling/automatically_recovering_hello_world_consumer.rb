8bb49ceaa18efc38fb7146a346751cbc71aa5546
Michael S. Klishin <michael@novemberain.com>
2011-06-28 03:41:32 +0400

    One more example: automatically recovering 'Hello, world' consumer + auxiliary producer script

---
+++ examples/error_handling/automatically_recovering_hello_world_consumer.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bundler"
+Bundler.setup
+
+$:.unshift(File.expand_path("../../../lib", __FILE__))
+
+require 'amqp'
+
+
+puts "=> Example of automatic AMQP channel and queues recovery"
+puts
+AMQP.start(:host => "localhost") do |connection, open_ok|
+  connection.on_error do |ch, connection_close|
+    raise connection_close.reply_text
+  end
+
+  ch1 = AMQP::Channel.new(connection, 2, :auto_recovery => true)
+  ch1.on_error do |ch, channel_close|
+    raise channel_close.reply_text
+  end
+
+  connection.on_tcp_connection_loss do |conn, settings|
+    puts "[network failure] Trying to reconnect..."
+    conn.reconnect(false, 2)
+  end
+
+  ch1.queue("amqpgem.examples.queue3", :auto_delete => false, :durable => true).bind("amq.fanout").subscribe do |metadata, payload|
+    puts " => #{payload}"
+  end
+
+
+  show_stopper = Proc.new {
+    connection.disconnect { puts "Disconnected. Exiting…"; EventMachine.stop }
+  }
+
+  Signal.trap "TERM", show_stopper
+  Signal.trap "INT",  show_stopper
+  EM.add_timer(45, show_stopper)
+
+
+  puts "Connected, authenticated. To really exercise this example, shut AMQP broker down for a few seconds. If you don't it will exit gracefully in 45 seconds."
+end
---
+++ examples/error_handling/hello_world_producer.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bundler"
+Bundler.setup
+
+$:.unshift(File.expand_path("../../../lib", __FILE__))
+
+require 'amqp'
+
+
+puts "=> Auxiliary script that tests automatically recovering message consumer(s)"
+puts
+AMQP.start(:host => "localhost") do |connection, open_ok|
+  connection.on_error do |ch, connection_close|
+    raise connection_close.reply_text
+  end
+
+  ch1 = AMQP::Channel.new(connection, 2)
+  ch1.on_error do |ch, channel_close|
+    raise channel_close.reply_text
+  end
+
+
+  exchange = ch1.fanout("amq.fanout", :durable => true)
+  EventMachine.add_periodic_timer(0.5) do
+    puts "Publishing..."
+    # messages must be routable & there must be at least one consumer.
+    exchange.publish("Hello", :immediate => true, :mandatory => true)
+  end
+
+
+  show_stopper = Proc.new {
+    connection.disconnect { puts "Disconnected. Exiting…"; EventMachine.stop }
+  }
+
+  Signal.trap "TERM", show_stopper
+  Signal.trap "INT",  show_stopper
+  EM.add_timer(15, show_stopper)
+end
