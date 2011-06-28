9e3c533a63c2da4183b892fe27c17d0c0e37ca78
Michael S. Klishin <michael@novemberain.com>
2011-06-28 05:10:48 +0400

    One more scenario

--- spec/integration/declare_and_immediately_bind_a_server_named_queue_spec.rb
+++ spec/integration/declare_and_immediately_bind_a_server_named_queue_spec.rb
@@ -12,31 +12,52 @@ describe "Server-named", AMQP::Queue do
 
   default_timeout 2
 
-  amqp_before do
-    @channel = AMQP::Channel.new
-  end
-
 
   #
   # Examples
   #
 
 
-  it "delays binding until after queue.declare-ok arrives" do
-    mailbox  = []
-    exchange = @channel.fanout("amq.fanout")
-    input    = "Independencia de resolución, ¿una realidad en Mac OS X Lion?"
+  context "bound to a pre-defined exchange" do
+    it "delays binding until after queue.declare-ok arrives" do
+      mailbox  = []
+      channel  = AMQP::Channel.new
+      exchange = channel.fanout("amq.fanout")
+      input    = "Independencia de resolución, ¿una realidad en Mac OS X Lion?"
 
-    @channel.queue("", :auto_delete => true).bind(exchange).subscribe do |header, body|
-      mailbox << body
+      channel.queue("", :auto_delete => true).bind(exchange).subscribe do |header, body|
+        mailbox << body
+      end
+
+      delayed(0.5) {
+        exchange.publish(input)
+      }
+
+      done(1.0) {
+        mailbox.size.should == 1
+      }
     end
+  end
 
-    delayed(0.5) {
-      exchange.publish(input)
-    }
 
-    done(1.0) {
-      mailbox.size.should == 1
-    }
+  context "bound to a pre-defined exchange" do
+    it "delays binding until after queue.declare-ok arrives" do
+      mailbox  = []
+      channel  = AMQP::Channel.new
+      exchange = channel.fanout("amqpgem.examples.exchanges.fanout", :durable => false)
+      input    = "Just send me already"
+
+      channel.queue("", :auto_delete => true).bind(exchange).subscribe do |header, body|
+        mailbox << body
+      end
+
+      delayed(0.5) {
+        exchange.publish(input)
+      }
+
+      done(1.0) {
+        mailbox.size.should == 1
+      }
+    end
   end
 end
