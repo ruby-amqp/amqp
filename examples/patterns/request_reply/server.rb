147b95f1797d4d1ce6ca17ed3753fe564858d94f
Michael S. Klishin <michael@novemberain.com>
2011-06-30 09:20:15 +0400

    First bits of the "Patterns & Use Cases" doc guide

--- examples/patterns/request_reply/server.rb
+++ examples/patterns/request_reply/server.rb
@@ -4,24 +4,22 @@ $LOAD_PATH.unshift File.expand_path("../../../../lib", __FILE__)
 
 require "amqp"
 
-t = Thread.new { EventMachine.run }
-sleep(0.5)
+EventMachine.run do
+  connection = AMQP.connect
+  channel    = AMQP::Channel.new(connection)
 
-connection = AMQP.connect
-channel    = AMQP::Channel.new(connection)
+  requests_queue = channel.queue("amqpgem.examples.services.time", :exclusive => true, :auto_delete => true)
+  requests_queue.subscribe(:ack => true) do |metadata, payload|
+    puts "[requests] Got a request #{metadata.message_id}. Sending a reply..."
+    channel.default_exchange.publish(Time.now.to_s,
+                                     :routing_key    => metadata.reply_to,
+                                     :correlation_id => metadata.message_id,
+                                     :immediate      => true)
 
-requests_queue = channel.queue("amqpgem.examples.services.time", :exclusive => true, :auto_delete => true)
-requests_queue.subscribe(:ack => true) do |metadata, payload|
-  puts "[requests] Got a request #{metadata.message_id}. Sending a reply..."
-  channel.default_exchange.publish(Time.now.to_s,
-                                   :routing_key    => metadata.reply_to,
-                                   :correlation_id => metadata.message_id,
-                                   :immediate      => true)
+    metadata.ack
+  end
 
-  metadata.ack
+
+
+  Signal.trap("INT") { connection.close { EventMachine.stop } }
 end
-
-
-
-Signal.trap("INT") { connection.close { EventMachine.stop } }
-t.join
