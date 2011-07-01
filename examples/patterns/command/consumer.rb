004a0bef55732ebc2f0ad4a98430c77c18d90a5a
Michael S. Klishin <michael@novemberain.com>
2011-07-01 09:07:26 +0400

    Mostly done with the command pattern

--- examples/patterns/command/consumer.rb
+++ examples/patterns/command/consumer.rb
@@ -1,8 +1,9 @@
-# encoding: utf - 8
+# encoding: utf-8
 
 $LOAD_PATH.unshift File.expand_path("../../../../lib", __FILE__)
 
 require "amqp"
+require "yaml"
 
 t = Thread.new { EventMachine.run }
 sleep(0.5)
@@ -17,20 +18,28 @@ channel.prefetch(1)
 # that the task is finished. If the consumer doesn't send
 # the acknowledgement, then the task is considered to be unfinished
 # and will be requeued when consumer closes AMQP connection (because of a crash, for example).
-channel.queue("amqpgem.examples.tasks", :durable => true, :auto_delete => false).subscribe(:ack => true) do |metadata, payload|
-  puts payload
-  puts system(payload)
+channel.queue("amqpgem.examples.patterns.command", :durable => true, :auto_delete => false).subscribe(:ack => true) do |metadata, payload|
+  case metadata.type
+  when "gems.install"
+    data = YAML.load(payload)
+    puts "[gems.install] Received a 'gems.install' request with #{data.inspect}"
 
-  puts
-  puts "[done] Done with #{payload}"
-  puts
+    # just to demonstrate a realistic example
+    shellout = "gem install #{data[:gem]} --version '#{data[:version]}'"
+    puts "[gems.install] Executing #{shellout}"; system(shellout)
 
-  # message is processed, acknowledge it so that broker
-  # removes it
+    puts
+    puts "[gems.install] Done"
+    puts
+  else
+    puts "[commands] Unknown command: #{metadata.type}"
+  end
+
+  # message is processed, acknowledge it so that broker discards it
   metadata.ack
 end
 
-
+puts "[boot] Ready"
 Signal.trap("INT") { connection.close { EventMachine.stop } }
 t.join
 
--- examples/patterns/command/producer.rb
+++ examples/patterns/command/producer.rb
@@ -1,8 +1,9 @@
-# encoding: utf - 8
+# encoding: utf-8
 
 $LOAD_PATH.unshift File.expand_path("../../../../lib", __FILE__)
 
 require "amqp"
+require "yaml"
 
 t = Thread.new { EventMachine.run }
 sleep(0.5)
@@ -10,14 +11,16 @@ sleep(0.5)
 connection = AMQP.connect
 channel    = AMQP::Channel.new(connection)
 
-
-command = "df -h"
-
 # publish new commands every 3 seconds
-EventMachine.add_periodic_timer(3.0) do
-  puts "Publishing a command (#{command})"
-  channel.default_exchange.publish(command, :routing_key => "amqpgem.examples.tasks")
+EventMachine.add_periodic_timer(10.0) do
+  puts "Publishing a command (gems.install)"
+  payload = { :gem => "rack", :version => "~> 1.3.0" }.to_yaml
+
+  channel.default_exchange.publish(payload,
+                                   :type        => "gems.install",
+                                   :routing_key => "amqpgem.examples.patterns.command")
 end
 
+puts "[boot] Ready. Will be publishing commands every 10 seconds."
 Signal.trap("INT") { connection.close { EventMachine.stop } }
 t.join
