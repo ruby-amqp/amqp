375208824c9eccb6312ceea25a7b436ed0a8b801
Michael S. Klishin <michael@novemberain.com>
2011-07-07 09:14:38 +0400

    More headers exchange routing examples

--- spec/integration/headers_exchange_routing_spec.rb
+++ spec/integration/headers_exchange_routing_spec.rb
@@ -90,55 +90,180 @@ end
 
 
 
-
-describe "Multiple consumers bound to a queue with the same headers" do
-
-  #
-  # Environment
-  #
-
+describe "Multiple consumers" do
   include EventedSpec::AMQPSpec
   default_options AMQP_OPTS
   default_timeout 5
 
-  amqp_before do
-    @channel   = AMQP::Channel.new
-    @channel.on_error do |ch, close|
-      raise "Channel-level error!: #{close.inspect}"
+  describe "bound to a queue with the same single header" do
+
+    #
+    # Environment
+    #
+
+    amqp_before do
+      @channel   = AMQP::Channel.new
+      @channel.on_error do |ch, close|
+        raise "Channel-level error!: #{close.inspect}"
+      end
+
+      @queue    = @channel.queue("", :auto_delete => true)
+      @exchange = @channel.headers("amqpgem.tests.integration.headers.exchange1", :auto_delete => true)
+
+      @queue.bind(@exchange, :arguments => { :slug => "all" })
     end
 
-    @queue    = @channel.queue("", :auto_delete => true)
-    @exchange = @channel.headers("amqpgem.tests.integration.headers.exchange1", :auto_delete => true)
 
-    @queue.bind(@exchange, :arguments => { :slug => "all" })
+
+    it "get messages distributed to them in a round-robin manner" do
+      mailbox1  = Array.new
+      mailbox2  = Array.new
+
+      consumer1 = AMQP::Consumer.new(@channel, @queue).consume.on_delivery { |metadata, payload| mailbox1 << payload }
+      consumer2 = AMQP::Consumer.new(@channel, @queue).consume.on_delivery { |metadata, payload| mailbox2 << payload }
+
+
+      EventMachine.add_timer(0.5) do
+        12.times { @exchange.publish(".", :headers => { :slug => "all" }) }
+        12.times { @exchange.publish(".", :headers => { :slug => "rspec" }) }
+      end
+
+      done(1.5) {
+        mailbox1.size.should == 6
+        mailbox2.size.should == 6
+      }
+    end
   end
 
 
 
-  it "get messages distributed to them in a round-robin manner" do
-    mailbox1  = Array.new
-    mailbox2  = Array.new
+  describe "bound to a queue with the same two header & x-match = all" do
 
-    consumer1 = AMQP::Consumer.new(@channel, @queue).consume
-    consumer2 = AMQP::Consumer.new(@channel, @queue).consume
+    #
+    # Environment
+    #
 
+    amqp_before do
+      @channel   = AMQP::Channel.new
+      @channel.on_error do |ch, close|
+        raise "Channel-level error!: #{close.inspect}"
+      end
 
-    consumer1.on_delivery do |metadata, payload|
-      mailbox1 << payload
-    end
-    consumer2.on_delivery do |metadata, payload|
-      mailbox2 << payload
-    end
+      @queue    = @channel.queue("", :auto_delete => true)
+      @exchange = @channel.headers("amqpgem.tests.integration.headers.exchange1", :auto_delete => true)
 
-
-    EventMachine.add_timer(0.5) do
-      12.times { @exchange.publish(".", :headers => { :slug => "all" }) }
-      12.times { @exchange.publish(".", :headers => { :slug => "rspec" }) }
+      @queue.bind(@exchange, :arguments => { :slug => "all", :arch => "ia64", 'x-match' => 'all' })
     end
 
-    done(1.5) {
-      mailbox1.size.should == 6
-      mailbox2.size.should == 6
-    }
+
+    it "get messages distributed to them in a round-robin manner" do
+      mailbox1  = Array.new
+      mailbox2  = Array.new
+
+      consumer1 = AMQP::Consumer.new(@channel, @queue).consume.on_delivery { |metadata, payload| mailbox1 << payload }
+      consumer2 = AMQP::Consumer.new(@channel, @queue).consume.on_delivery { |metadata, payload| mailbox2 << payload }
+
+      EventMachine.add_timer(0.5) do
+        12.times { @exchange.publish(".", :headers => { :slug => "all",   :arch => "ia64" }) }
+        12.times { @exchange.publish(".", :headers => { :slug => "rspec", :arch => "ia64" }) }
+      end
+
+      done(1.5) {
+        mailbox1.size.should == 6
+        mailbox2.size.should == 6
+      }
+    end
+  end
+
+
+
+  describe "bound to 2 queues with the same two header & x-match = all" do
+
+    #
+    # Environment
+    #
+
+    amqp_before do
+      @channel   = AMQP::Channel.new
+      @channel.on_error do |ch, close|
+        raise "Channel-level error!: #{close.inspect}"
+      end
+
+      @queue1   = @channel.queue("", :auto_delete => true)
+      @queue2   = @channel.queue("", :auto_delete => true)
+      @exchange = @channel.headers("amqpgem.tests.integration.headers.exchange1", :auto_delete => true)
+
+      args      = { :slug => "all", :arch => "ia64", 'x-match' => 'all' }
+      @queue1.bind(@exchange, :arguments => args)
+      @queue2.bind(@exchange, :arguments => args)
+    end
+
+    it "get messages distributed to both queues, and in a round-robin manner between consumers on one queue" do
+      mailbox1  = Array.new
+      mailbox2  = Array.new
+      mailbox3  = Array.new
+      mailbox4  = Array.new
+
+      consumer1 = AMQP::Consumer.new(@channel, @queue1).consume.on_delivery { |metadata, payload| mailbox1 << payload }
+      consumer2 = AMQP::Consumer.new(@channel, @queue1).consume.on_delivery { |metadata, payload| mailbox2 << payload }
+      consumer3 = AMQP::Consumer.new(@channel, @queue2).consume.on_delivery { |metadata, payload| mailbox3 << payload }
+      consumer4 = AMQP::Consumer.new(@channel, @queue2).consume.on_delivery { |metadata, payload| mailbox4 << payload }
+
+      EventMachine.add_timer(0.5) do
+        12.times { |i| @exchange.publish("all-#{i}",   :headers => { :slug => "all",   :arch => "ia64" }) }
+        16.times { |i| @exchange.publish("rspec-#{i}", :headers => { :slug => "rspec", :arch => "ia64" }) }
+      end
+
+      done(1.5) {
+        mailbox1.size.should == 6
+        mailbox1.should == ["all-0", "all-2", "all-4", "all-6", "all-8", "all-10"]
+
+        mailbox2.size.should == 6
+        mailbox2.should == ["all-1", "all-3", "all-5", "all-7", "all-9", "all-11"]
+
+        mailbox3.size.should == 6
+        mailbox4.size.should == 6
+      }
+    end
+  end
+
+
+
+
+  describe "bound to a queue with the same two header & x-match = any" do
+
+    #
+    # Environment
+    #
+
+    amqp_before do
+      @channel   = AMQP::Channel.new
+      @channel.on_error do |ch, close|
+        raise "Channel-level error!: #{close.inspect}"
+      end
+
+      @queue    = @channel.queue("", :auto_delete => true)
+      @exchange = @channel.headers("amqpgem.tests.integration.headers.exchange1", :auto_delete => true)
+
+      @queue.bind(@exchange, :arguments => { :slug => "all", :arch => "ia64", 'x-match' => 'any' })
+    end
+
+    it "get messages distributed to them in a round-robin manner" do
+      mailbox1  = Array.new
+      mailbox2  = Array.new
+
+      consumer1 = AMQP::Consumer.new(@channel, @queue).consume.on_delivery { |metadata, payload| mailbox1 << payload }
+      consumer2 = AMQP::Consumer.new(@channel, @queue).consume.on_delivery { |metadata, payload| mailbox2 << payload }
+
+      EventMachine.add_timer(0.5) do
+        12.times { @exchange.publish(".", :headers => { :slug => "all",   :arch => "ia64" }) }
+        4.times  { @exchange.publish(".", :headers => { :slug => "rspec", :arch => "ia64" }) }
+      end
+
+      done(1.5) {
+        mailbox1.size.should == 8
+        mailbox2.size.should == 8
+      }
+    end
   end
 end
