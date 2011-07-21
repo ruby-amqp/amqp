4937a02b2decb3feebb0092c8ef550bed9e06964
Michael S. Klishin <michael@novemberain.com>
2011-07-21 08:26:01 +0400

    Formatting

--- examples/hello_world_with_an_empty_string.rb
+++ examples/hello_world_with_an_empty_string.rb
@@ -25,9 +25,7 @@ EventMachine.run do
   queue.subscribe do |payload|
     puts "Received a message: #{payload}. Disconnecting..."
 
-    connection.close {
-      EM.stop { exit }
-    }
+    connection.close { EventMachine.stop }
   end
 
   exchange.publish "", :routing_key => queue.name, :app_id => "Hello world"
