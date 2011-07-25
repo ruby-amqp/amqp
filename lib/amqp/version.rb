abdcb247276c389b79ed0e56ba64eeb53e6e7312
Michael S. Klishin <michael@novemberain.com>
2011-07-25 06:59:43 +0400

    0.8.0.rc14

--- lib/amqp/version.rb
+++ lib/amqp/version.rb
@@ -1,10 +1,10 @@
 # encoding: utf-8
 
 module AMQP
-  # AMQP gem version. Not to be confused with AMQP protocol version
+  # amqp gem version. Not to be confused with the AMQP protocol version
   # it implements. For that, see {AMQ::Protocol::VERSION}
   #
   # @see AMQ::Protocol::VERSION
   # @return [String] AMQP gem version
-  VERSION = '0.8.0.rc14.pre'
+  VERSION = '0.8.0.rc14'
 end
