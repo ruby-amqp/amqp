d99cfcd9ea91a81da28f174987339d478c80a43c
Michael S. Klishin <michael@novemberain.com>
2011-07-29 08:19:00 +0400

    Revisit Per-queue Message TTL extension example

--- examples/extensions/rabbitmq/per_queue_message_ttl.rb
+++ examples/extensions/rabbitmq/per_queue_message_ttl.rb
@@ -10,44 +10,43 @@ require "amqp/extensions/rabbitmq"
 
 AMQP.start do |connection|
   puts "Connected!"
-  AMQP::Channel.new(connection) do |channel, open_ok|
-    puts "Channel #{channel.id} is now open"
+  channel = AMQP::Channel.new(connection)
+  channel.on_error do |ch, channel_close|
+    puts "Oops! a channel-level exception: #{channel_close.reply_text}"
+  end
 
-    channel.on_error do |ch, channel_close|
-      puts "Oops! a channel-level exception: #{channel_close.reply_text}"
+  x = channel.fanout("amq.fanout")
+  channel.queue("", :auto_delete => true, :arguments => { "x-message-ttl" => 1000 }) do |q|
+    puts "Declared a new server-named queue: #{q.name}"
+    q.bind(x)
+
+
+    EventMachine.add_timer(0.3) do
+      10.times do |i|
+        puts "Publishing message ##{i}"
+        x.publish("Message ##{i}")
+      end
     end
 
-    x = channel.fanout("amq.fanout")
-    channel.queue("", :auto_delete => true, :arguments => { "x-message-ttl" => 1000 }) do |q|
-      puts "Declared a new server-named qeueue: #{q.name}"
-      q.bind(x)
-
-
-      EventMachine.add_timer(0.3) do
-        10.times do |i|
-          puts "Publishing message ##{i}"
-          x.publish("Message ##{i}")
-        end
+    EventMachine.add_timer(0.7) do
+      q.pop do |headers, payload|
+        puts "Got a message: #{payload}"
       end
+    end
 
-      EventMachine.add_timer(0.7) do
-        q.pop do |headers, payload|
-          raise "x-message-ttl didn't seem to work (timeout is up)" if payload.nil?
-        end
-      end
-
-      EventMachine.add_timer(1.5) do
-        q.pop do |headers, payload|
-          raise "x-message-ttl didn't seem to work (timeout isn't up)" if payload
+    EventMachine.add_timer(1.5) do
+      q.pop do |headers, payload|
+        if payload.nil?
+          puts "No messages in the queue"
+        else
+          raise "x-message-ttl didn't seem to work (timeout isn't up)"
         end
       end
     end
   end
 
   show_stopper = Proc.new {
-    AMQP.stop do
-      EM.stop
-    end
+    AMQP.stop { EventMachine.stop }
   }
 
 
