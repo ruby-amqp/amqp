ee5e6529c7b5c3113a1a93cd329c473b1c958213
Michael S. Klishin <michael@novemberain.com>
2011-08-20 11:33:50 +0400

    Blow off the dust from the publisher confirms code example

--- examples/extensions/rabbitmq/publisher_confirmations_with_transient_messages.rb
+++ examples/extensions/rabbitmq/publisher_confirmations_with_transient_messages.rb
@@ -8,46 +8,37 @@ $:.unshift(File.expand_path("../../../../lib", __FILE__))
 require 'amqp'
 require "amqp/extensions/rabbitmq"
 
-AMQP.start do |connection|
-  puts "Connected!"
+EventMachine.run do
+  connection = AMQP.connect(:host => '127.0.0.1')
+  puts "Connecting to AMQP broker. Running #{AMQP::VERSION} version of the gem..."
+
   AMQP::Channel.new(connection) do |channel|
     puts "Channel #{channel.id} is now open"
 
     channel.confirm_select
-    channel.on_error do |ch, channel_close|
-      puts "Oops! a channel-level exception: #{channel_close.reply_text}"
-    end
-
-
-    channel.on_ack do |basic_ack|
-      puts "Received basic_ack: multiple = #{basic_ack.multiple}, delivery_tag = #{basic_ack.delivery_tag}"
-    end
+    channel.on_error { |ch, channel_close| puts "Oops! a channel-level exception: #{channel_close.reply_text}" }
+    channel.on_ack   { |basic_ack| puts "Received basic_ack: multiple = #{basic_ack.multiple}, delivery_tag = #{basic_ack.delivery_tag}" }
 
     x = channel.fanout("amq.fanout")
     channel.queue("", :auto_delete => true) do |q|
-      puts "Declared a new server-named qeueue: #{q.name}"
-
-      q.bind(x, :no_ack => true).subscribe(:ack => true) do |header, payload|
+      q.bind(x).subscribe(:ack => true) do |header, payload|
         puts "Received #{payload}"
       end
     end
 
     EventMachine.add_timer(0.5) do
       10.times do |i|
-        puts "Publishing message ##{i}"
-        x.publish("Message ##{i}")
+        puts "Publishing message ##{i + 1}"
+        x.publish("Message ##{i + 1}")
       end
     end
   end
 
   show_stopper = Proc.new {
-    AMQP.stop do
-      EM.stop
-    end
+    connection.close { EventMachine.stop }
   }
 
-
-  EM.add_timer(3, show_stopper)
+  EM.add_timer(6, show_stopper)
   Signal.trap('INT',  show_stopper)
   Signal.trap('TERM', show_stopper)
 end
