61cbd4d6a65e3bb2dc36d712546f04ee0a95e2ef
Michael S. Klishin <michael@novemberain.com>
2011-09-03 12:37:00 +0400

    Add basic spec example that lets delivery tags grow beyond 192, references #107

---
+++ spec/integration/message_acknowledgement_spec.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe "Message acknowledgements" do
+
+  #
+  # Environment
+  #
+
+  include EventedSpec::AMQPSpec
+
+  default_timeout 15
+
+  amqp_before do
+    @connection = AMQP.connect
+    @channel1   = AMQP::Channel.new(@connection)
+    @channel2   = AMQP::Channel.new(@connection)
+  end
+
+
+  it "can be issued for delivery tags >= 192" do
+    exchange_name = "amqpgem.tests.fanout#{rand}"
+    queue         = @channel1.queue("", :auto_delete => true).bind(exchange_name).subscribe(:ack => true) do |metadata, payload|
+      puts "x-sequence = #{metadata.headers['x-sequence']}, delivery_tag = #{metadata.delivery_tag}" if ENV["DEBUG"]
+      metadata.ack
+      if metadata.delivery_tag >= 999
+        done(1.0)
+      end
+    end
+
+    exchange = @channel2.fanout(exchange_name, :durable => false)
+    2000.times do |i|
+      exchange.publish("", :headers => { 'x-sequence' => i })
+    end
+  end
+end
