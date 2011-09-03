06a0a89bc37d818baf36446f62040b1cafa8c330
Michael S. Klishin <michael@novemberain.com>
2011-09-03 13:00:05 +0400

    Exclude growing delivery tags example from CI

--- spec/integration/message_acknowledgement_spec.rb
+++ spec/integration/message_acknowledgement_spec.rb
@@ -2,36 +2,38 @@
 
 require 'spec_helper'
 
-describe "Message acknowledgements" do
+unless ENV["CI"]
+  describe "Message acknowledgements" do
 
-  #
-  # Environment
-  #
+    #
+    # Environment
+    #
 
-  include EventedSpec::AMQPSpec
+    include EventedSpec::AMQPSpec
 
-  default_timeout 120
+    default_timeout 120
 
-  amqp_before do
-    @connection = AMQP.connect
-    @channel1   = AMQP::Channel.new(@connection)
-    @channel2   = AMQP::Channel.new(@connection)
-  end
+    amqp_before do
+      @connection = AMQP.connect
+      @channel1   = AMQP::Channel.new(@connection)
+      @channel2   = AMQP::Channel.new(@connection)
+    end
 
 
-  it "can be issued for delivery tags >= 192" do
-    exchange_name = "amqpgem.tests.fanout#{rand}"
-    queue         = @channel1.queue("", :auto_delete => true).bind(exchange_name).subscribe(:ack => true) do |metadata, payload|
-      puts "x-sequence = #{metadata.headers['x-sequence']}, delivery_tag = #{metadata.delivery_tag}" if ENV["DEBUG"]
-      metadata.ack
-      if metadata.delivery_tag >= 999
-        done(1.0)
+    it "can be issued for delivery tags >= 192" do
+      exchange_name = "amqpgem.tests.fanout#{rand}"
+      queue         = @channel1.queue("", :auto_delete => true).bind(exchange_name).subscribe(:ack => true) do |metadata, payload|
+        puts "x-sequence = #{metadata.headers['x-sequence']}, delivery_tag = #{metadata.delivery_tag}" if ENV["DEBUG"]
+        metadata.ack
+        if metadata.delivery_tag >= 999
+          done(1.0)
+        end
+      end
+
+      exchange = @channel2.fanout(exchange_name, :durable => false)
+      2000.times do |i|
+        exchange.publish("", :headers => { 'x-sequence' => i })
       end
     end
-
-    exchange = @channel2.fanout(exchange_name, :durable => false)
-    2000.times do |i|
-      exchange.publish("", :headers => { 'x-sequence' => i })
-    end
-  end
+  end  
 end
