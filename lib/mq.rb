65c69642acae9adf01dd2f364d2548788dc2404a
Michael S. Klishin <michael@novemberain.com>
2011-09-04 15:12:42 +0400

    Update deprecation warning for require 'mq'

--- lib/mq.rb
+++ lib/mq.rb
@@ -19,11 +19,13 @@ Why is it deprecated? Because it was a poor name choice all along. We better not
 release 1.0 with it. Both mq.rb and MQ class step away from AMQP terminology and
 make 8 out of 10 engineers think it has something to do with AMQP queues (in fact,
 MQ should have been called Channel all along). No other AMQP client library we know
-of invents it's own terminology when it comes to AMQP entities, and amqp gem shouldn't,
+of invents its own terminology when it comes to AMQP entities, and amqp gem shouldn't,
 too.
 
 Learn more at http://bit.ly/amqp-gem-080-migration, all documentation guides are at
-http://bit.ly/amqp-gem-docs.
+http://bit.ly/amqp-gem-docs, AMQP Model Explained at http://bit.ly/amqp-model-explained.
+
+We are also in #rabbitmq on irc.freenode.net.
 
 Thank you for understanding. AMQP gem maintainers team.
 
