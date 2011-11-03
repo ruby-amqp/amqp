50d15f19971daf2f289cb527922298a4a94ab947
Michael S. Klishin <michael@novemberain.com>
2011-11-03 19:54:40 +0400

    Support :uri option in Rails integration (config/amqp.yml)
    Example:
    
    development:
      uri: amqp://myapp:myapp_password@localhost/my.app.development
    
    in addition to already supported
    
    development:
    
      vhost: my.app.development
      user: myapp
      password: myapp_password
    
    [ci skip]

--- lib/amqp/integration/rails.rb
+++ lib/amqp/integration/rails.rb
@@ -4,12 +4,18 @@ module AMQP
   module Integration
     class Rails
 
-      def self.start(options = {}, &block)
+      def self.start(options_or_uri = {}, &block)
         yaml     = YAML.load_file(File.join(::Rails.root, "config", "amqp.yml"))
         settings = yaml.fetch(::Rails.env, Hash.new).symbolize_keys
 
+        arg      = if options_or_uri.is_a?(Hash)
+                     settings.merge(options_or_uri)[:uri]
+                   else
+                     settings[:uri] || options_or_uri
+                   end
+
         EventMachine.next_tick do
-          AMQP.start(settings.merge(options), &block)
+          AMQP.start(arg, &block)
         end
       end
     end # Rails
