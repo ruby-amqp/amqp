9051eea444016cd7febc69526cac1eb1fd8bd6f4
Richard Heycock <rgh@roughage.com.au>
2012-04-04 15:26:17 +1000

    validate_parameters_match! checks are too aggresive.
    According to the AMQP 0.9.1 spec if a queue is re-declared the following
    parameters should be checked:
    
      :durable, :exclusive, :auto_delete, :arguments
    
    and if an exchange is re-declaring then the following parameters should
    be checked:
    
      :type, :durable, :arguments
    
    Before this patch the entire parameters hash was being checked which
    caused valid paramters to raise an AMQP::IncompatibleOptionsError
    exception.

--- spec/integration/queue_redeclaration_with_incompatible_attributes_spec.rb
+++ spec/integration/queue_redeclaration_with_incompatible_attributes_spec.rb
@@ -20,14 +20,17 @@ describe AMQP do
   context "when queue is redeclared with different attributes" do
     let(:name)              { "amqp-gem.nondurable.queue" }
     let(:options)           {
-      { :durable => false, :passive => false }
+      { :durable => false, :exclusive => true, :auto_delete => true, :arguments => {}, :passive => false }
     }
     let(:different_options) {
-      { :durable => true, :passive => false}
+      { :durable => true, :exclusive => true, :auto_delete => true, :arguments => {}, :passive => false}
+    }
+    let(:irrelevant_different_options) {
+      { :durable => false, :exclusive => true, :auto_delete => true, :arguments => {}, :passive => false, :header => {:random => 'stuff' } }
     }
 
 
-    it "should raise AMQP::IncompatibleOptionsError" do
+    it "should raise AMQP::IncompatibleOptionsError for incompatable options" do
       channel = AMQP::Channel.new
       channel.on_error do |ch, close|
         @callback_fired = true
@@ -37,6 +40,19 @@ describe AMQP do
       expect {
         channel.queue(name, different_options)
       }.to raise_error(AMQP::IncompatibleOptionsError)
+      done
+    end
+
+    it "should not raise AMQP::IncompatibleOptionsError for irrelevant options" do
+      channel = AMQP::Channel.new
+      channel.on_error do |ch, close|
+        @callback_fired = true
+      end
+
+      channel.queue(name, options)
+      expect {
+        channel.queue(name, irrelevant_different_options)
+      }.to_not raise_error(AMQP::IncompatibleOptionsError)
       done
     end
   end
