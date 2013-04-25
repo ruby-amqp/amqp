2f2226ecc14a51493fcc2933155be1aa39f5a126
Michael Klishin <michael@defprotocol.org>
2013-04-25 14:48:15 +0400

    Update channel reopening example to use the channel after it has been reopened
    This revealed a number of issues, some of which have been corrected in amq-client 1.0.1
    and at least one is still TBD.

--- examples/error_handling/reopening_a_channel_after_channel_level_exception.rb
+++ examples/error_handling/reopening_a_channel_after_channel_level_exception.rb
@@ -12,8 +12,8 @@ require 'amqp'
 puts "=> Queue redeclaration with different attributes results in a channel exception that is handled"
 puts
 EventMachine.run do
-  connection = AMQP.connect
-  AMQP::Channel.new(connection, :auto_recovery => true) do |channel, open_ok|
+  connection = AMQP.connect(:port => ENV.fetch("PORT", 5672))
+  AMQP::Channel.new(connection) do |channel, open_ok|
     puts "Channel ##{channel.id} is now open!"
 
     connection.on_error do |conn, connection_close|
@@ -25,31 +25,30 @@ EventMachine.run do
     end
 
     channel.on_error do |ch, channel_close|
-      puts <<-ERR
-      Handling a channel-level exception.
-
-      AMQP class id : #{channel_close.class_id},
-      AMQP method id: #{channel_close.method_id},
-      Status code   : #{channel_close.reply_code}
-      Error message : #{channel_close.reply_text}
-      ERR
-
-      puts "Reusing channel #{ch.id}"
+      puts "Handling a channel-level exception: #{channel_close.reply_code} #{channel_close.reply_text}"
       ch.reuse
-      puts "Channel id is now #{ch.id}"
     end
 
-    EventMachine.add_timer(1.0) do
+    # initial operations that cause the channel-level exception handled above
+    EventMachine.add_timer(0.5) do
       # these two definitions result in a race condition. For sake of this example,
       # however, it does not matter. Whatever definition succeeds first, 2nd one will
       # cause a channel-level exception (because attributes are not identical)
-      AMQP::Queue.new(channel, "amqpgem.examples.channel_exception", :auto_delete => true, :durable => false) do |queue|
-        puts "#{queue.name} is ready to go"
-      end
+      AMQP::Queue.new(channel, "amqpgem.examples.channel_exception", :exclusive => true, :durable => false)
+      AMQP::Queue.new(channel, "amqpgem.examples.channel_exception", :auto_delete => true, :durable => true)
+    end
 
-      AMQP::Queue.new(channel, "amqpgem.examples.channel_exception", :auto_delete => true, :durable => true) do |queue|
-        puts "#{queue.name} is ready to go"
+    EventMachine.add_timer(2.0) do
+      puts "Resuming with reopened channel #{channel.id}"
+      q = channel.queue("amqpgem.examples.channel_exception.q1", :exclusive => true).bind("amq.fanout").subscribe do |meta, payload|
+        puts "Consumed #{payload}"
       end
+    end
+
+    EventMachine.add_timer(3.0) do
+      puts "Publishing a message..."
+      x = channel.fanout("amq.fanout")
+      x.publish("xyz", :routing_key => "amqpgem.examples.channel_exception.q1")
     end
   end
 
