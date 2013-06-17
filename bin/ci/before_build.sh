ec73c7fb798702d702268362ec77ebfe16ace2e6
Michael Klishin <michael@defprotocol.org>
2013-06-17 18:37:45 +0400

    Allow overriding rabbitmqctl
    Makes it possible to develop against a development instance of rabbitmq

--- bin/ci/before_build.sh
+++ bin/ci/before_build.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 
+${RABBITMQCTL:="$RABBITMQCTL"}
+${RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
+
 # guest:guest has full access to /
 
-sudo rabbitmqctl add_vhost /
-sudo rabbitmqctl add_user guest guest
-sudo rabbitmqctl set_permissions -p / guest ".*" ".*" ".*"
+$RABBITMQCTL add_vhost /
+$RABBITMQCTL add_user guest guest
+$RABBITMQCTL set_permissions -p / guest ".*" ".*" ".*"
 
 
 # amqp_gem:amqp_gem_password has full access to amqp_gem_testbed
 
-sudo rabbitmqctl add_vhost amqp_gem_testbed
-sudo rabbitmqctl add_user amqp_gem amqp_gem_password
-sudo rabbitmqctl set_permissions -p amqp_gem_testbed amqp_gem ".*" ".*" ".*"
+$RABBITMQCTL add_vhost amqp_gem_testbed
+$RABBITMQCTL add_user amqp_gem amqp_gem_password
+$RABBITMQCTL set_permissions -p amqp_gem_testbed amqp_gem ".*" ".*" ".*"
 
 
 # amqp_gem_reader:reader_password has read access to amqp_gem_testbed
 
-sudo rabbitmqctl add_user amqp_gem_reader reader_password
-sudo rabbitmqctl clear_permissions -p amqp_gem_testbed guest
-sudo rabbitmqctl set_permissions -p amqp_gem_testbed amqp_gem_reader "^---$" "^---$" ".*"
+$RABBITMQCTL add_user amqp_gem_reader reader_password
+$RABBITMQCTL clear_permissions -p amqp_gem_testbed guest
+$RABBITMQCTL set_permissions -p amqp_gem_testbed amqp_gem_reader "^---$" "^---$" ".*"
+
+# requires RabbitMQ 3.0+
+# $RABBITMQ_PLUGINS enable rabbitmq_consistent_hash_exchange
