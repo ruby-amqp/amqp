84aadef8ec2e88ea136d0250774b21989da5fbbb
Michael Klishin <michael@defprotocol.org>
2013-06-29 01:36:34 +0400

    All extensions stuff is already in the core

--- lib/amq/client/extensions/rabbitmq.rb
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-
-# http://www.rabbitmq.com/extensions.html
-require "amq/client/extensions/rabbitmq/basic"
-require "amq/client/extensions/rabbitmq/cancel"
-require "amq/client/extensions/rabbitmq/confirm"
