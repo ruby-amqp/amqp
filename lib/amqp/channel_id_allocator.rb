5270bc0b5242e2beb18561a7066a7d07ce3c42ea
Neo Cincinnati <pairing@edgecase.com>
2013-12-03 15:51:26 -0500

    Support 65535 channels per Session, not per process (like Bunny).

---
+++ lib/amqp/channel_id_allocator.rb
@@ -0,0 +1,58 @@
+module AMQP
+  module ChannelIdAllocator
+
+    MAX_CHANNELS_PER_CONNECTION = (2**16)
+
+    # Resets channel allocator. This method is thread safe.
+    # @api public
+    # @see Channel.next_channel_id
+    # @see Channel.release_channel_id
+    def reset_channel_id_allocator
+      channel_id_mutex.synchronize do
+        int_allocator.reset
+      end
+    end
+
+    # Releases previously allocated channel id. This method is thread safe.
+    #
+    # @param [Fixnum] Channel id to release
+    # @api public
+    # @see Channel.next_channel_id
+    # @see Channel.reset_channel_id_allocator
+    def release_channel_id(i)
+      channel_id_mutex.synchronize do
+        int_allocator.release(i)
+      end
+    end
+
+    # Returns next available channel id. This method is thread safe.
+    #
+    # @return [Fixnum]
+    # @api public
+    # @see Channel.release_channel_id
+    # @see Channel.reset_channel_id_allocator
+    def next_channel_id
+      channel_id_mutex.synchronize do
+        result = int_allocator.allocate
+        raise "No further channels available. Please open a new connection." if result < 0
+        result
+      end
+    end
+
+    private
+
+    # @private
+    # @api private
+    def channel_id_mutex
+      @channel_id_mutex ||= Mutex.new
+    end
+
+    # @private
+    def int_allocator
+      # TODO: ideally, this should be in agreement with agreed max number of channels of the connection,
+      #       but it is possible that value either not yet available. MK.
+      @int_allocator ||= IntAllocator.new(1, MAX_CHANNELS_PER_CONNECTION)
+    end
+
+  end
+end
--- spec/unit/amqp/channel_id_allocation_spec.rb
+++ spec/unit/amqp/channel_id_allocation_spec.rb
@@ -1,40 +1,60 @@
 # encoding: utf-8
 
 require "spec_helper"
+describe AMQP::ChannelIdAllocator do
 
-describe AMQP::Channel do
-  describe ".next_channel_id" do
-    before :all do
-      described_class.reset_channel_id_allocator
+  class ChannelAllocator
+    include AMQP::ChannelIdAllocator
+  end
+
+  describe "#next_channel_id" do
+    subject do
+      ChannelAllocator.new()
     end
 
     context "when there is a channel id available for allocation" do
       it "returns that channel id" do
-        1024.times { described_class.next_channel_id }
+        1024.times { subject.next_channel_id }
 
-        described_class.next_channel_id.should == 1025
+        subject.next_channel_id.should == 1025
       end
     end
 
     context "when THERE IS NOT channel id available for allocation" do
-      it "raises an exception"
+      it "raises an exception" do
+        (ChannelAllocator::MAX_CHANNELS_PER_CONNECTION - 1).times do
+          subject.next_channel_id
+        end
+
+        lambda { subject.next_channel_id }.should raise_error
+      end
     end
   end
 
 
-
   describe ".release_channel_id" do
-    before :all do
-      described_class.reset_channel_id_allocator
+    subject do
+      ChannelAllocator.new()
     end
 
     it "releases that channel id" do
-      1024.times { described_class.next_channel_id }
-      described_class.next_channel_id.should == 1025
+      1024.times { subject.next_channel_id }
+      subject.next_channel_id.should == 1025
 
-      described_class.release_channel_id(128)
-      described_class.next_channel_id.should == 128
-      described_class.next_channel_id.should == 1026
+      subject.release_channel_id(128)
+      subject.next_channel_id.should == 128
+      subject.next_channel_id.should == 1026
+    end
+  end
+
+  describe "each instance gets its own channel IDs" do
+    it "has an allocator per instance" do
+      one = ChannelAllocator.new()
+      two = ChannelAllocator.new()
+      one.next_channel_id.should == 1
+      one.next_channel_id.should == 2
+      two.next_channel_id.should == 1
+      two.next_channel_id.should == 2
     end
   end
 end
